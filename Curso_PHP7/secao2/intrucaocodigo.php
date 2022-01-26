86a198401bbeb301db1bfff5f364168223f98206
fipewmc <fipewmc2@gmail.com>
2022-01-26 10:38:59 +0000

    Instruções de código - Seção 2 - Conceitos Básicos

--- Curso_PHP7/secao2/intrucaocodigo.php
+++ Curso_PHP7/secao2/intrucaocodigo.php
@@ -1,5 +1,8 @@
 <?php
     echo "uma linha <br>";
-    echo "Duas Linhas <br>";
+    echo "Duas Linhas <br><br>";
 
-    
+    if( 5 > 2){
+        echo "O 5 é maior que 2 <br>";
+        echo "Estou dentro de um if <br>";
+    }
