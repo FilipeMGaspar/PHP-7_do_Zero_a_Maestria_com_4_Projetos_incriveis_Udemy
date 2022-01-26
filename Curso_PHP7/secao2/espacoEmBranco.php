6eab993807b71b366393941bc6827f58184a887c
fipewmc <fipewmc2@gmail.com>
2022-01-26 10:45:12 +0000

    Espaços em branco - Seção 2 - Conceitos Básicos

--- Curso_PHP7/secao2/espacoEmBranco.php
+++ Curso_PHP7/secao2/espacoEmBranco.php
@@ -1,4 +1,8 @@
 <?php   
     echo        "<br>  Testando espaço em Branco <br>";
     echo "Mais um teste de espaço em branco <br>"                ;
-    
+    echo
+    "Quebra de Linha <br>";
+    echo "Quebra de linho "
+    ;
+
