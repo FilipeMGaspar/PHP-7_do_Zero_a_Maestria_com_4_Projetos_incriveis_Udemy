780b59032bc50959fce40d891ff90fafc86170b4
fipewmc <fipewmc2@gmail.com>
2022-01-27 11:11:23 +0000

    Exercício 4 - Seção 3 - Tipos de Dados

---
+++ Curso_PHP7/secao3/Exercicio_4.php
@@ -0,0 +1,16 @@
+<?php
+$num 0 0.123456789;
+
+    echo 12.236589;
+    echo "<br>";
+    echo 1.25;
+    echo "<br>";
+    echo $num;
+
+    echo "<br><br>O $num ";
+
+    if (is_float($num)){
+        echo "É um número Décimal!";
+    }else {
+        echo "NÃO É UM número Décimal!";
+    }
