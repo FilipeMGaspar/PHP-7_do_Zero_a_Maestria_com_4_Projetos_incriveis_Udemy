14ef270b650aaadb35545221c5f0ac058b87f9d1
fipewmc <fipewmc2@gmail.com>
2022-01-27 11:03:53 +0000

    Num Decimais - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/numDecimal.php
+++ Curso_PHP7/secao3/numDecimal.php
@@ -1,8 +1,8 @@
 <?php
-    $num1 = 5.6598;
-    $num2 = 10.23;
+    $num1 = 5.235;
+    $num2 = 10.895;
 
-    echo 156.23589;
+    echo 256.235;
     echo "<br>";
     echo $num1;
     echo "<br>";
