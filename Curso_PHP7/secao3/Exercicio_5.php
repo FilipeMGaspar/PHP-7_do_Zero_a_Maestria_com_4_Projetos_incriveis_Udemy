d3800de03bb627f83f3a3e2df2219d0c4bc6e844
fipewmc <fipewmc2@gmail.com>
2022-02-01 16:21:10 +0000

    Exercício 5 - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/Exercicio_5.php
+++ Curso_PHP7/secao3/Exercicio_5.php
@@ -2,7 +2,7 @@
     $str = "'Sou um texto'";
 
     echo "Texto com aspas duplas!<br><br>";
-    echo 'Texto com aspas simples';
+    echo 'Texto com aspas simples<br><br>';
 
     echo $str;
     echo 'Bulgatos citou "Sou, dona da treteca!"';
