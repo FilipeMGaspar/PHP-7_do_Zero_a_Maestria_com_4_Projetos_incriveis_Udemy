6556b3da22da4123c8b2505713d8131780577240
fipewmc <fipewmc2@gmail.com>
2022-02-01 16:17:41 +0000

    Verificando se é String - Seção 3 - Tipos de Dados

---
+++ Curso_PHP7/secao3/e_string.php
@@ -0,0 +1,9 @@
+<?php
+
+$texto = "Olá Mundo";
+
+if(is_string($texto)){
+    echo "$texto é uma String (cadeia de carateres) <br><br>";
+}else{
+    echo "$texto não é uma String (cadeia de carateres) <br><br>";
+}
