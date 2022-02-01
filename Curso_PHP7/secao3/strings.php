8c6ae67d817a039533a8735afed9f322f5f9427b
fipewmc <fipewmc2@gmail.com>
2022-02-01 16:11:58 +0000

    Strings - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/strings.php
+++ Curso_PHP7/secao3/strings.php
@@ -3,4 +3,8 @@
 
     echo "Olá Mundo! <br>";
     echo "Sou $nome de PHP7 <br>";
-    echo 'Texto com aspas simples';
+    echo 'Texto com aspas simples <br>';
+
+    echo "Ele disse 'Ser ou não ser!' <br>";
+    $idade = 18;
+
