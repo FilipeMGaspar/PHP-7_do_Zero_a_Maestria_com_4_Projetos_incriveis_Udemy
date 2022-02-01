ed0bc439c94d335151a436e3953976c6a19c9e2d
fipewmc <fipewmc2@gmail.com>
2022-02-01 16:13:54 +0000

    Strings - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/strings.php
+++ Curso_PHP7/secao3/strings.php
@@ -1,10 +1,17 @@
 <?php
     $nome = "Estudante";
 
-    echo "Olá Mundo! <br>";
-    echo "Sou $nome de PHP7 <br>";
-    echo 'Texto com aspas simples <br>';
+    echo "Olá Mundo! <br><br>";
+    echo "Sou $nome de PHP7 <br><br>";
+    echo 'Texto com aspas simples <br><br>';
 
-    echo "Ele disse 'Ser ou não ser!' <br>";
+    echo "Ele disse 'Ser ou não ser!' <br><br>";
+
+    echo 'Mas o outro disse "Penso logo existo!"<br><br>';
+
     $idade = 18;
+
+    echo "O João tem $idade anos! <br><br>";
+     
+
 
