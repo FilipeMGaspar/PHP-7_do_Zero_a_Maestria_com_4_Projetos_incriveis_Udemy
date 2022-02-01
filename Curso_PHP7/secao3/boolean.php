a94050fb8c2972e36f9040cfbd5785bec834fa96
fipewmc <fipewmc2@gmail.com>
2022-02-01 16:29:26 +0000

    Boolean - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/boolean.php
+++ Curso_PHP7/secao3/boolean.php
@@ -5,3 +5,11 @@
 
     echo "$verdadeiro <br><br>";
     echo "$falso <br>";
+
+    if(true){
+        echo "É Verdadeiro! <br><br>";
+    }
+
+    if(5 > 2){
+        echo "É Verdadeiro! <br><br>";
+    }
