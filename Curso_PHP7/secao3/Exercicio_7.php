4def7a8561c2ea39954dcdd3e38dc88285c8a088
fipewmc <fipewmc2@gmail.com>
2022-02-08 12:05:20 +0000

    Exercício 7 - Seção 3 - Tipos de Dados

---
+++ Curso_PHP7/secao3/Exercicio_7.php
@@ -0,0 +1,11 @@
+<?php
+
+$pessoa =[
+'nome' => 'Lucas',
+'idade' => 25,
+'Nacionalidade' => "Francesa",
+"Nif" => 12301230
+];
+
+echo "<br>";
+echo "O Sr. " . $pessoa['nome'] . " de nacionalidade " . $pessoa['Nacionalidade'];
