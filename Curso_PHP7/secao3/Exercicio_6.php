12b1f386a913e9ff9561e871b0687127b42a052c
fipewmc <fipewmc2@gmail.com>
2022-02-08 11:41:57 +0000

    Exercício 6 - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/Exercicio_6.php
+++ Curso_PHP7/secao3/Exercicio_6.php
@@ -1,2 +1,2 @@
 <?php
-$carro = ['Marca' => "Ford", 'Modelo' => "Mondeo", 'Combustivel' => 'Diesel']
+$carro = ['Marca' => "Ford", 'Modelo' => "Mondeo", 'Combustivel' => 'Diesel', 'Motor' => 1.9, 'Cvs' => 130];
