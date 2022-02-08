682dd4e0737d7eefc36f078562c272f4a67fd8b9
fipewmc <fipewmc2@gmail.com>
2022-02-08 10:17:11 +0000

    Arrays Associativo - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/arrayAssociativo.php
+++ Curso_PHP7/secao3/arrayAssociativo.php
@@ -3,7 +3,7 @@ $arr = ['nome' => 'Marcos', 'idade' => 35];
 
 print_r($arr);
 
-echo "<br><br>$arr['nome]";
+echo "<br><br>$arr['nome']";
 echo "<br><br>$arr['idade']";
 
 $arrAssoc ['chave' => 'Valor', 'bool' => true];
