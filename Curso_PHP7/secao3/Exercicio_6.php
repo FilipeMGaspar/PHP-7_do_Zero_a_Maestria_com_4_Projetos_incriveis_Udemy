9b2e7daddbe690817f3d46f134556324576ee947
fipewmc <fipewmc2@gmail.com>
2022-02-08 11:45:35 +0000

    Exercício 6 - Seção 3 - Tipos de Dados

--- Curso_PHP7/secao3/Exercicio_6.php
+++ Curso_PHP7/secao3/Exercicio_6.php
@@ -1,2 +1,8 @@
 <?php
 $carro = ['Marca' => "Ford", 'Modelo' => "Mondeo", 'Combustivel' => 'Diesel', 'Motor' => 1.9, 'Cvs' => 130];
+
+echo "<br><br>";
+echo "Marca:       $carro['Marca']<br>";
+echo "Modelo:      $carro['Modelo']<br>";
+echo "Combustivel: $carro['Combustivel']<br>";
+echo "Motor:       $carro['Motor'] TDI de $carro['Cvs'] Cavalos";
