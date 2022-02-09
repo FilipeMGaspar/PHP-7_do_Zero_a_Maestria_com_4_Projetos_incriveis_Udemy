ecc50fb66d4dda19ad7148f826dce7a7db646f59
fipewmc <fipewmc2@gmail.com>
2022-02-09 16:02:48 +0000

    Declarando variáveis- Seção 4 - Variáveis

---
+++ Curso_PHP7/secao4/variaveis.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nome = "Lucas";
+
+    echo "$nome";
+
+    echo "<br><br>";
+
+    $nome = "Claudia";
+
+    echo "$nome";
+
+    echo "<br><br>";
+
+    $nome = 36;
+
+    echo "$nome";
