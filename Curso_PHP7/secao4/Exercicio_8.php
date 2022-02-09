f27b7797b9c009bfa942f4ea1a3e5ac48a82fa79
fipewmc <fipewmc2@gmail.com>
2022-02-09 16:12:14 +0000

    Exercício 9 - Seção 4 - Variáveis

---
+++ Curso_PHP7/secao4/Exercicio_8.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = "Paulo Couves";
+    $numMatricula = 202135269;
+    $curso = "Tim - Técnologias de Informação e Multimédia";
+    $propinas = 1250.45;
+
+    echo "O aluno $nome com a matricula $numMatricula do curso $curso <br><br>";
+
+    
