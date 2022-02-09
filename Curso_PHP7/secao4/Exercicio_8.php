ad84537a5496f3d0c96b4108cc3249395325bdfd
fipewmc <fipewmc2@gmail.com>
2022-02-09 16:14:05 +0000

    Exercício 9 - Seção 4 - Variáveis

--- Curso_PHP7/secao4/Exercicio_8.php
+++ Curso_PHP7/secao4/Exercicio_8.php
@@ -5,6 +5,7 @@
     $curso = "Tim - Técnologias de Informação e Multimédia";
     $propinas = 1250.45;
 
-    echo "O aluno $nome com a matricula $numMatricula do curso $curso <br><br>";
+    echo "O aluno $nome <br> Matricula $numMatricula 
+    <br>Curso $curso <br><br>";
 
     echo "Tem de propinas $propinas €";
