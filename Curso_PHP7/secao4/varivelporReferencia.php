48b74490645c0f624a1317e26fa283330f856a92
fipewmc <fipewmc2@gmail.com>
2022-02-10 12:39:49 +0000

    Variável por referência - Seção 4 - Variáveis

--- Curso_PHP7/secao4/varivelporReferencia.php
+++ Curso_PHP7/secao4/varivelporReferencia.php
@@ -8,9 +8,14 @@
     echo "$y<br><br>";
 
     $y = 15;
-    echo "Atribuição após ref!";
+    echo "Atribuição após ref!<br><br>";
     echo "$x<br><br>";
 
     echo "$y<br><br>";
 
-    
+    echo "Atribuição após ref2!<br><br>";
+
+    $x = 20;
+    echo "$x<br><br>";
+
+    echo "$y<br><br>";
