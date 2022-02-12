1d72e2734b97793ad9738e9355b5a0d5432b0a2e
fipewmc <fipewmc2@gmail.com>
2022-02-12 19:37:51 +0000

    Parâmetros de Função - Seção 4 - Variáveis

--- Curso_PHP7/secao4/parametrosDeFuncao.php
+++ Curso_PHP7/secao4/parametrosDeFuncao.php
@@ -1,5 +1,13 @@
 <?php
 
     function somar($n1, $n2){
-        $soma = $n1 + $n2;
+        echo $n1 + $n2;
+
+        echo "<br>";
     }
+
+    somar(2, 4);
+
+    somar(6, 8);
+
+    somar(10, 10);
