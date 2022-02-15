d06fdd5903fbdaf1040ebb71c813585058225cec
fipewmc <fipewmc2@gmail.com>
2022-02-15 09:43:49 +0000

    Exercício 11 - Seção 5 - Expressões e Operadores

--- Curso_PHP7/secao5/exercicio_11.php
+++ Curso_PHP7/secao5/exercicio_11.php
@@ -5,3 +5,17 @@ $expressao = "5" * 12;
 echo $expressao . "<br>";
 
 echo gettype($expressao);
+
+echo "<br><br>";
+
+echo gettype([]);
+
+echo "<br><br>";
+
+echo gettype(12.2);
+
+echo "<br><br>";
+
+echo gettype("Teste");
+
+echo "<br><br>";
