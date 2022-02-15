d3c574a5d7c354897b9c0684c0ad9603c41a83ba
fipewmc <fipewmc2@gmail.com>
2022-02-15 09:33:38 +0000

    Mudança de tipo implícito - Seção 5 - Expressões e Operadores

--- Curso_PHP7/secao5/mudandoTipoImplicito.php
+++ Curso_PHP7/secao5/mudandoTipoImplicito.php
@@ -1,12 +1,18 @@
 <?php
 echo 5 / 2;
-echo "<br><br>";
+echo "<br>";
 
 if(is_float(5/2)){
-    echo "è um float";
+    echo "É um float!";
 }
 
 
 echo "<br><br>";
 
 echo 2 . 3;
+
+echo "<br>";
+
+if (is_string(2.3)){
+    echo "É uma String!";
+}
