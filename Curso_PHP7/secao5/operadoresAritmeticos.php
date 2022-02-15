01afefa2e16b8114b1c8d422385598646f144291
fipewmc <fipewmc2@gmail.com>
2022-02-15 09:48:25 +0000

    Operadores aritméticos - Seção 5 - Expressões e Operadores

--- Curso_PHP7/secao5/operadoresAritmeticos.php
+++ Curso_PHP7/secao5/operadoresAritmeticos.php
@@ -9,3 +9,8 @@ echo "Soma = " . $soma . "<br><br>";
 
 $subtract = $num1 - $num2;
 
+echo "Subtração = " . $subtract . "<br><br>";
+
+$multiplica = $num1 * $num2;
+
+echo "Multiplicação = " . $multiplica . "<br><br>";
