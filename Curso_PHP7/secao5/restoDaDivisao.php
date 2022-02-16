fab56d45fd89c34f0912bef384a0cf4b5eb59ef6
fipewmc <fipewmc2@gmail.com>
2022-02-16 12:15:36 +0000

    Resto da divisão (Operador Módulo)- Seção 5 - Expressões

--- Curso_PHP7/secao5/restoDaDivisao.php
+++ Curso_PHP7/secao5/restoDaDivisao.php
@@ -1,7 +1,8 @@
 <?php
 
-echo 12 % 2 . " Módulo" . "<br><br>";
+echo 12 % 2 . " Módulo - resto da divisão de 12 por 2" . "<br><br>";
 
-echo 12 / 2 . " Divisão"."<br><br>";
+echo 12 / 2 . " Divisão de 12 por 2 "."<br><br>";
 
-echo 13 % 2 . " Módulo" . "<br><br>";
+echo 13 % 2 . " Módulo - resto da divisão de 13 por 2" . "<br><br>";
+
