b3c02ce9e4e100493ab3737123a83e4076df1c54
fipewmc <fipewmc2@gmail.com>
2022-02-16 12:27:47 +0000

    Exercício 13 - Seção 5 - Expressões

--- Curso_PHP7/secao5/Exercicio_13.php
+++ Curso_PHP7/secao5/Exercicio_13.php
@@ -3,6 +3,6 @@ $num = 5;
 $num1 = 25;
 $num2 = 4;
 
-echo "Resto da dividão de $num1 de $num é igual a " . $num1 % $num . "<br><br>";
+echo "Resto da dividão de $num1 de $num = " . $num1 % $num . "<br><br>";
 
-echo "Resto da dividão de $num2 de $num é igual a " . $num2 % $num . "<br><br>";
+echo "Resto da dividão de $num2 de $num = " . $num2 % $num . "<br><br>";
