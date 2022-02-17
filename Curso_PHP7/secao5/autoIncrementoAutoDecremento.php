55f1861eee56b1eeb506235c20c1c746a1936dad
fipewmc <fipewmc2@gmail.com>
2022-02-17 16:17:03 +0000

    Auto incremento e auto decremento - Seção 5 - Expressões

--- Curso_PHP7/secao5/autoIncrementoAutoDecremento.php
+++ Curso_PHP7/secao5/autoIncrementoAutoDecremento.php
@@ -6,11 +6,14 @@ $x = 5;
 
 echo "Auto incremento! $n <br><br>";
 
+$n++;
 echo $n++; //$n = $n + 1;
 
 echo "<br><br>";
 
 
 echo "Auto decremento! $x <br><br>";
+
+$x--;
 
 echo $x--; //$x = $x -1;
