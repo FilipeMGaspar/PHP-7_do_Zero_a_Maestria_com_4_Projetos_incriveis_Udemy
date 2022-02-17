e7010b6eb1af684f637df00774529133121b7d0e
fipewmc <fipewmc2@gmail.com>
2022-02-17 16:26:37 +0000

    Operadores de igualdade - Seção 5 - Expressões

--- Curso_PHP7/secao5/operadorDeIgualdade.php
+++ Curso_PHP7/secao5/operadorDeIgualdade.php
@@ -1,9 +1,13 @@
 <?php
 $num = 5;
-$comp = 6;
+$comp = 5;
 
 if($num == $comp){
     echo "$num  é igual a $comp";
-}else{
-    echo "$num Não é igual a $comp";
+}
+
+$num = 10;
+
+if ($num == $comp){
+    echo "$num não é igual a $comp";
 }
