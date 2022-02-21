a28623e4ace2100fad1f371b15ef7731d89904fb
fipewmc <fipewmc2@gmail.com>
2022-02-21 21:12:15 +0000

    Operador lógico AND (E)- Seção 5 - Expressões

--- Curso_PHP7/secao5/opLogicoAND.php
+++ Curso_PHP7/secao5/opLogicoAND.php
@@ -1,9 +1,13 @@
 <?php
 
 if(5 > 10 && 10 > 5){ //false e true
-    echo "Entrei no primeiro if 5 > 10 && 10 > 5";
+    echo "Entrei no primeiro if '5 > 10 && 10 > 5'";
 }
 
 if(5 < 10 && 10 > 5){ //true e true
     echo "Entrei no primeiro if '5 < 10 && 10 > 5' <br><br>";
+}
+
+if(5 < 10 && 10 > 50){ //true e false
+    echo "Entrei no primeiro if '5 < 10 && 10 > 50' <br><br>";
 }
