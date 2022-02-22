1f86736548e01c9cd7dbcc57b5c6a20c1c730fa4
fipewmc <fipewmc2@gmail.com>
2022-02-22 16:36:15 +0000

    Teste de PHP e JavaScript - Seção 5 - Expressões

--- Curso_PHP7/secao5/testeMInes.php
+++ Curso_PHP7/secao5/testeMInes.php
@@ -1,2 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    let nome = pronpt("Qual é o teu nome? ");
+    alert "Olá " + nome + " seja bem vindo!";;
+    </script>
 <?php
-echo '<script> alert ("olá Mundo!") </script>';
+    echo '<script> alert ("olá Mundo!") </script>';
+?>    
+</body>
+</html>
+
+
