edbce968ef4743d90499383c4598dccb50c4435e
fipewmc <fipewmc2@gmail.com>
2022-02-23 09:21:33 +0000

    Operador lógico OR - Seção 5 - Expressões

--- Curso_PHP7/secao5/opLogicoOR.php
+++ Curso_PHP7/secao5/opLogicoOR.php
@@ -25,6 +25,6 @@ if($a > $b || $d < $c){ //false   ou   true   = true
     echo "<br> $a > $b || $d < $c <br><br>";
 }
 
-if(($a > $b || $d < $c) && $c < $d)){ //true e true = true
+if(($a > $b || $d > $c) && $c < $d){ //true e true = true
     echo "<br> ($a > $b || $d < $c) && $c < $d <br><br>";
 }
