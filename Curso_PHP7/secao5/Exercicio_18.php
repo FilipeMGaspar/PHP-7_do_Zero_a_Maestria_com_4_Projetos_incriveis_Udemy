b42794a0f46efef932494c6cb6b0bb97e705bdd8
fipewmc <fipewmc2@gmail.com>
2022-02-23 09:28:03 +0000

    Exercício 18 - Seção 5 - Expressões

--- Curso_PHP7/secao5/Exercicio_18.php
+++ Curso_PHP7/secao5/Exercicio_18.php
@@ -8,3 +8,7 @@ $nome = "joao";
 if($a < $b || "joao" === $nome){ //false   ou true  = true
     echo "<br> $a < $b || 'joao' === $nome <br><br>";
 }
+
+if(1 > 5 || 1){
+    echo "<br> 1 > 5 || 1 <br><br>";
+}
