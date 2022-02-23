a5e63d0b506ba944b640c7ff47dd2e51249e49f4
fipewmc <fipewmc2@gmail.com>
2022-02-23 09:37:12 +0000

    Operador lógico NOT - Seção 5 - Expressões

--- Curso_PHP7/secao5/opLogicoNOT.php
+++ Curso_PHP7/secao5/opLogicoNOT.php
@@ -1,9 +1,13 @@
 <?php
 
     if(5 > 2) { //true
-        echo "<br> 5 > 2 <br><br>";
+        echo "<br> 5 é > 2 <br><br>";
     }
 
     if(!(5 > 2)) { //false
-        echo "<br> 5 > 2 <br><br>";
+        echo "<br> 5 não é > 2 <br><br>";
+    }
+
+    if(!(50 > 2)) { //false
+        echo "<br> 50 não é > 2 <br><br>";
     }
