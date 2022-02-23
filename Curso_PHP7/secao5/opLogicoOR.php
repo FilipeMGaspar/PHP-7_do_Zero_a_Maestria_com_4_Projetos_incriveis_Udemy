cb3a4dab8ff3f28c875ed9d827c1465806d2cae3
fipewmc <fipewmc2@gmail.com>
2022-02-23 09:14:34 +0000

    Operador lógico OR - Seção 5 - Expressões

--- Curso_PHP7/secao5/opLogicoOR.php
+++ Curso_PHP7/secao5/opLogicoOR.php
@@ -1,9 +1,13 @@
 <?php
 
-if(5 > 15 || "teste" == "teste"){ //false  ou true = true
+if(5 > 15 || "teste" == "teste"){ //false  ou  true  =  true
     echo "<br> 5 > 15 || 'teste' == 'teste' <br><br>";
 }
 
-if(5 > 2 || 3 < 4){ //true ou true  =  true
+if(5 > 2 || 3 < 4){ //true  ou  true  =  true
     echo "<br> 5 > 2 || 3 < 4 <br><br>";
+}
+
+if(5 > 20 || 3 < 4){ //false  ou  true  =  true
+    echo "<br> 5 > 20 || 3 < 4 <br><br>";
 }
