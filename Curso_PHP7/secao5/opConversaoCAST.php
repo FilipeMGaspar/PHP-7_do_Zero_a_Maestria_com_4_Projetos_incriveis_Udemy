d05234d07cb48428174eba0919752ba03587bba3
fipewmc <fipewmc2@gmail.com>
2022-02-24 16:05:24 +0000

    Operador de Conversão (cast)- Seção 5 - Expressões

--- Curso_PHP7/secao5/opConversaoCAST.php
+++ Curso_PHP7/secao5/opConversaoCAST.php
@@ -13,3 +13,9 @@ echo "<br><br>";
 if($a === 12){
     echo "$a é idêntico a 12 <br><br>";
 }
+
+$b = (float) 3.14151896;
+
+if($b === 3.14151896){
+    echo "$b é idêntico a 3.14151896 <br><br>";
+}
