cbf5a88592f26f3bccc8939991515e71a90ddf7d
fipewmc <fipewmc2@gmail.com>
2022-02-25 11:48:23 +0000

    Operador ternário - Seção 5 - Expressões

--- Curso_PHP7/secao5/opTernario.php
+++ Curso_PHP7/secao5/opTernario.php
@@ -1,6 +1,6 @@
 <?php
     //   comp            true                    false
-    echo 5 > 2 ? "5 é maior que 2 <br>" : "5 é menor que 2 <br>";
+    echo 5 > 2 ? "5 é maior que 2 <br><br>" : "5 é menor que 2 <br><br>";
 
 
-    echo 20 > 50 ? "20 é maior que 50 <br>" : "20 é menor que 50 <br>";
+    echo 20 > 50 ? "20 é maior que 50 <br><br>" : "20 é menor que 50 <br><br>";
