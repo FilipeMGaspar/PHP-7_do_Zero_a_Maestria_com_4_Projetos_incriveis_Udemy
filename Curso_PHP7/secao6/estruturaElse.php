ecd275d7b02d0646c58a8cdf1bb3f314f5fdcdd8
fipewmc <fipewmc2@gmail.com>
2022-02-28 09:56:10 +0000

    Estrutura else - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/estruturaElse.php
+++ Curso_PHP7/secao6/estruturaElse.php
@@ -3,10 +3,10 @@
 $a = 2;
 $b = 5;
 
-    if($a > $b) {
+    if($a > $b) {  // 2 é menor que 5 não entra no if
         echo "$a é maior  que $b";
         echo "<br><br>";
-    } else{
+    } else{ //False 2 é menor que 5 entra no else
         echo "$b é maior que $a";
         echo "<br><br>";
     }
