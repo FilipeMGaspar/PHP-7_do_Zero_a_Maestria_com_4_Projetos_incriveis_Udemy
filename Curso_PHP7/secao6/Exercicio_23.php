c06549aa79b044b2a9775412fc71511a6d529fbc
fipewmc <fipewmc2@gmail.com>
2022-02-28 10:00:51 +0000

    Exercício 23 - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/Exercicio_23.php
+++ Curso_PHP7/secao6/Exercicio_23.php
@@ -5,6 +5,9 @@
     if($idade >= 18){
         echo "Você tem $idade, logo é maior de Idade!";
         echo "<br><br>";
+    } else {
+        echo "Você tem $idade, logo é menor de Idade!"; 
+        echo "<br><br>";
     }
 
     $idade = 25;
