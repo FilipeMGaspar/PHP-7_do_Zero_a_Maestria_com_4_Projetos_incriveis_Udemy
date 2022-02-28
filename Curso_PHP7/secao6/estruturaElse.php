d975ac5dda9a674fdb98ed410599ee1b710333ee
fipewmc <fipewmc2@gmail.com>
2022-02-28 09:53:31 +0000

    Estrutura else - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/estruturaElse.php
+++ Curso_PHP7/secao6/estruturaElse.php
@@ -4,8 +4,9 @@ $a = 2:
 $b = 5;
 
     if($a > $b){
-        echo "";
+        echo "$a é maior  que $b";
         echo "<br><br>";
     } else{
+        echo "$b é maior que $a";
         echo "<br><br>";
     }
