00e6e31db4d4a364a7162d494fbe085a11404aa1
fipewmc <fipewmc2@gmail.com>
2022-03-01 07:51:06 +0000

    Exercício 24 - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/Exercicio_24.php
+++ Curso_PHP7/secao6/Exercicio_24.php
@@ -7,9 +7,9 @@ $txt = "Texto";
 $check = true;
 
 if(is_int($num)){
-    echo "O número $num, <strong>É</strong> um número <strong>inteiro!</strong>";
+    echo "O número $num, <strong>É</strong> um número <strong>INTEIRO!</strong>";
     echo "<br><br>";
 } else {
-    echo "O número $num, <strong>NÃO É</strong> um número <strong>inteiro!</strong>";
+    echo "O número $num, <strong>NÃO É</strong> um número <strong>INTEIRO!</strong>";
     echo "<br><br>";
 }
