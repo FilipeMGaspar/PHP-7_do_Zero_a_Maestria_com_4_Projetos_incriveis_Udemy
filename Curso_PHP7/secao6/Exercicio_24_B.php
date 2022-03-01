9ac5b1fcb9098de5fe700885b6d0e28aaab69827
fipewmc <fipewmc2@gmail.com>
2022-03-01 08:01:33 +0000

    Exercício 24 B - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/Exercicio_24_B.php
+++ Curso_PHP7/secao6/Exercicio_24_B.php
@@ -5,3 +5,13 @@ se peso maior que 80, imprima uma mensagem que está pesado de mais
 
 se não, imprima "Peso dentro do limite"
 */ 
+
+$peso = 80.2;
+
+if($peso > 80){
+    echo "o seu peso atual é de $peso Kg, infelizmento encontra-se acima do peso ideal!";
+    echo "<br><br>";
+} else {
+    echo "o seu peso atual é de $peso Kg, infelizmento encontra-se no peso ideal!";
+    echo "<br><br>";
+}
