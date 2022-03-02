d92408bde5ac47f117a9746c58eb99dc608a3d38
fipewmc <fipewmc2@gmail.com>
2022-03-02 09:18:04 +0000

    Exercício 9 - Seção 4 - Variáveis

--- Curso_PHP7/secao4/Exercicio_9.php
+++ Curso_PHP7/secao4/Exercicio_9.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Crie um arquivo PHP
+Crie duas variáveis com números
+crie uma terceira com a soma destes dois números
+Lembrendo: a soma pode ser feita com o símbolo +
+Ex: 2 + 4
+*/ 
 
     $num1 = 25.25;
     $num2 = 100.36;
