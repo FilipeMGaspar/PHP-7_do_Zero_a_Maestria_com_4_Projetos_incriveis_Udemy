5dcdb1e7da95ae4c401fa11382bd240044cec9e4
fipewmc <fipewmc2@gmail.com>
2022-03-02 09:33:00 +0000

    Exercício 1 - Seção 1 - Introdução

--- Curso_PHP7/secao1/Exercicio_1.php
+++ Curso_PHP7/secao1/Exercicio_1.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
-
+Crie uma arquivo PHP
+Este arquivo deve ter uma instrução que imprima o seu nome
+Execute o arquivo
 */
 
     echo "Olá eu sou o ALUNO, e estou a aprender PHP 7 com o Matheus Battisti";
