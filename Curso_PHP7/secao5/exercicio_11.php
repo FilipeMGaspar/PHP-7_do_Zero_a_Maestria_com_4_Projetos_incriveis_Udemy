3236619c00ff829486ffa0f2e84616dba110eae9
fipewmc <fipewmc2@gmail.com>
2022-03-02 09:13:13 +0000

    Exercício 11 - Seção 5 - Expressões

--- Curso_PHP7/secao5/exercicio_11.php
+++ Curso_PHP7/secao5/exercicio_11.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Crie um arquivo PHP
+Teste a expressão "5" * 12
+Utilize a função gettype() com o resultado como parâmetro para checar o tipo resultante da operação
+*/ 
 
 $expressao = "5" * 12;
 
