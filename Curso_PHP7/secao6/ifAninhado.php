e7305d48e856a7c2ac0ab81fc740d80fe7b622d4
fipewmc <fipewmc2@gmail.com>
2022-03-02 09:49:05 +0000

    If  aninhado - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/ifAninhado.php
+++ Curso_PHP7/secao6/ifAninhado.php
@@ -7,3 +7,13 @@
             echo "Entrou no Segundo if <br><br>";
         }
     }
+
+    if(10 > 2){
+        echo "Entrou no primeiro if <br><br>";
+
+        if("teste" != "teste"){
+            echo "Entrou no Segundo if <br><br>";
+        } else{
+            echo "Entrou no Segundo else <br><br>";
+        }
+    }
