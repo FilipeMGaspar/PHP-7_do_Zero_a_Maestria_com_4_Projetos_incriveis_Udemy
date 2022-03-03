87e3dc13bdf36ed14456930af603c7f9cba3db2e
fipewmc <fipewmc2@gmail.com>
2022-03-03 16:01:20 +0000

    Exercício 25 - Seção 6 - Estruturas de Controlo

---
+++ Curso_PHP7/secao6/Exercicio_25.php
@@ -0,0 +1,7 @@
+<?php
+
+    /*
+    Crie variáveis com números e outras com string
+    Faça um if checando se é um número, caso for, atribua a multiplicação deste número por 2 em outra variável
+    E crie um outro if, que checa se o novo número é maior que 100
+    */ 
