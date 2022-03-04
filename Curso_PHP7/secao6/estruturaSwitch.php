f75b5fbe08fcf1d473cb58cdc7766f2f17c7d507
fipewmc <fipewmc2@gmail.com>
2022-03-04 11:32:11 +0000

    Estrutura Switch - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/estruturaSwitch.php
+++ Curso_PHP7/secao6/estruturaSwitch.php
@@ -1,6 +1,6 @@
 <?php
 
-$x = 0;
+$x = 8;
 
 switch($x){
     case 0:
@@ -9,6 +9,7 @@ switch($x){
     case 1: 
         echo "X é igual a 1 <br><br>";
         break;
-
-
+    default:
+        echo "Valor inválido!<br><br>";
+        break;
 }
