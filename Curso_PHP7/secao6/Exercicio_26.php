a223996f1b9d1c8717ea2740f0e44a64372f2801
fipewmc <fipewmc2@gmail.com>
2022-03-04 09:30:23 +0000

    Exercício 26 - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/Exercicio_26.php
+++ Curso_PHP7/secao6/Exercicio_26.php
@@ -4,7 +4,7 @@
 Crie uma variável que recebe uma velocidade de um carro
 Depois crie uma estrutura if que verifica essa velocidade
 Se a velocidade for menor que 40, imprima que o motorista está na velocidade correta
-Se igual a 40, imprima uma mensagem para o motorista tomatr cuidado
+Se igual a 40, imprima uma mensagem para o motorista tomar cuidado
 Se for maior de 40, imprima uma mensagem de multa.
 */ 
 
@@ -13,4 +13,10 @@ $velAtualCarro = 35;
 if($velAtualCarro < 40){
     echo "Velocidade atual : $velAtualCarro Km/h - Velocidade correta";
     echo "<br><br>";
+} else if($velAtualCarro == 40){
+    echo "Velocidade atual : $velAtualCarro Km/h - Atensão radar";
+    echo "<br><br>";
+} else {
+    echo "Velocidade atual : $velAtualCarro Km/h -  Multado";
+    echo "<br><br>"; 
 }
