1e25bfcfb69e621fe8f64a17850878580d1bf549
fipewmc <fipewmc2@gmail.com>
2022-03-04 09:17:08 +0000

    Estrutura Else If - Seção 6 - Estruturas de Controlo

--- Curso_PHP7/secao6/estrururaElseIf.php
+++ Curso_PHP7/secao6/estrururaElseIf.php
@@ -35,3 +35,17 @@ if(10 < 5){
 }else {
     echo "Entrou no else <br><br>";
 }
+
+$a = 10;
+$b = 5;
+$msg = "Entrou no else if";
+
+if($a < $b){
+    echo "Entrou no if <br><br>";
+} else if($a < $b){
+    echo "Entrou no else if <br><br>";
+} else if($a > $b) {
+    echo "Entrou no else if 2 exec 4<br><br>";
+}else {
+    echo $msg . "<br><br>";
+}
