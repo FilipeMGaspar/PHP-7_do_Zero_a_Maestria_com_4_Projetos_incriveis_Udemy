244903c332447b7080b010a3d5a6f0f119412aac
fipewmc <fipewmc2@gmail.com>
2022-03-06 21:51:50 +0000

    Estrutura While- Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/estruturaWhile.php
+++ Curso_PHP7/secao7/estruturaWhile.php
@@ -17,3 +17,15 @@ while($y <= 10){
     $y += 2;
 }
 echo "FIM! <br><br>";
+
+$a = 10;
+
+while($a > 0){
+
+    if($a % 2 != 0){ //imprime apenas números impares
+        echo $a;
+        echo " - ";
+    }
+    $a--;
+}
+echo "FIM! <br><br>";
