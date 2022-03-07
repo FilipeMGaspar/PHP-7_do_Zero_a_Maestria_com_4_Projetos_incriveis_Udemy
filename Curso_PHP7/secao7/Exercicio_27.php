b9a649c7522a25e2b7424f567121ef8aefbbeba9
fipewmc <fipewmc2@gmail.com>
2022-03-07 09:15:28 +0000

    Exercício 27 - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/Exercicio_27.php
+++ Curso_PHP7/secao7/Exercicio_27.php
@@ -1,6 +1,6 @@
 <?php
 
-    $arr = ["Texto", 100, 12.6, 'c', true, "Melga", 78, "frase", 78.269, "String"];
+    $arr = ["Texto", 100, 12.6, 'c', true, "Melga", 78, "Frase", 78.269, "String"];
 
     $i = 0;
 
