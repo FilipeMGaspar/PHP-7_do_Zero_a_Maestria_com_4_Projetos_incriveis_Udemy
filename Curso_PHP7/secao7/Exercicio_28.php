097274e153be87dd645a29d342a804e18c385d74
fipewmc <fipewmc2@gmail.com>
2022-03-08 09:27:28 +0000

    Exercício 28 - Seção 7 - Estruturas de repetição

---
+++ Curso_PHP7/secao7/Exercicio_28.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+Crie um loop que vai até o número 30;
+O contador deve iniciar com 4
+Faça incrementos de 2 em 2 no contador
+Utilize o break para para o loop quando chegar ao número 24
+*/
