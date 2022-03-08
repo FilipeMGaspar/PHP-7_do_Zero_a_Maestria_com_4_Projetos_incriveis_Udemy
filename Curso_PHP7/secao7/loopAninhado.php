7e15046b4e5fa6ccebd3654c01234ea2c78e1196
fipewmc <fipewmc2@gmail.com>
2022-03-08 16:11:16 +0000

    Loop Aninhado - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/loopAninhado.php
+++ Curso_PHP7/secao7/loopAninhado.php
@@ -1,7 +1,15 @@
 <?php
 
-    $i = 0;
+    $i = 1;
 
-    while($i < 10){
+    while($i <= 10){
         echo "Loop Externo : $i<br><br>";
+        $i++;
+
+        //segundo contador
+        $j = 1;
+        while($j <= 5){
+            echo "Loop Externo : $j<br><br>";
+            $j++;
+        }
     }
