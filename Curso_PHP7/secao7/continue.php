2c3fd560145ed94e423e115a647507b7e23f1e55
fipewmc <fipewmc2@gmail.com>
2022-03-09 09:13:40 +0000

    Continue em loops - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/continue.php
+++ Curso_PHP7/secao7/continue.php
@@ -2,6 +2,11 @@
     $x = 10;
 
     while($x > 0){
+      
+
+       if($x == 5 || $x == 7){
+            echo "Pulou a execução $x <br>";
+       }
        echo "Executando o loop $x<br>";
 
        $x--;
