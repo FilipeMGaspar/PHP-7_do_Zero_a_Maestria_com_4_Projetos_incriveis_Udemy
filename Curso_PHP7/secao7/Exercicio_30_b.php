ecb3674aa27d95ee45859a783c4e49c5eccd7bfd
fipewmc <fipewmc2@gmail.com>
2022-03-10 15:45:09 +0000

    Exercício 30 B - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/Exercicio_30_b.php
+++ Curso_PHP7/secao7/Exercicio_30_b.php
@@ -1,1 +1,10 @@
 <?php
+
+/*
+    * Crie um array de 1 a 10
+    * Utilize um loop para criar este array
+    
+    ** Dica: você pode utilizar o método array_push(arr, elemento) para inserir um elemento num array
+
+    * Imprima o array com o print_r
+*/
