6ad552b28880b902249202ecba8e9b43002debb5
fipewmc <fipewmc2@gmail.com>
2022-03-10 15:26:58 +0000

    Estrutura FOR - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/estruturaFor.php
+++ Curso_PHP7/secao7/estruturaFor.php
@@ -1,6 +1,12 @@
 <?php
 
+    echo "<br>Incremento com a Estrutura For<br>";    
+
     for($i = 0; $i <10; $i++){
         echo "Testando a estrutura FOR $i";
         echo "<br>";
     }
+
+    echo "<br><br>";
+
+    echo "<br>Incremento com a Estrutura For<br>"; 
