3bdd7074f5dc21e0e86f864c14ca9162744872c0
fipewmc <fipewmc2@gmail.com>
2022-03-11 09:39:49 +0000

    Foreach - Seção 7 - Estruturas de repetição

--- Curso_PHP7/secao7/Estruturaforeach.php
+++ Curso_PHP7/secao7/Estruturaforeach.php
@@ -2,6 +2,9 @@
 
     $nomes = ["Matheus", "João", "Pedro", "Maria"];
 
+    foreach($nomes as $nome){
+        echo "O nome atual é $nome <br><br>";
+    }
     /*
       foreach($array as $item){
 
