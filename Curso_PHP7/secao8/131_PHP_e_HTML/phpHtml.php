050fc120ddf0205bde3db810be443160ffee9b1d
fipewmc <fipewmc2@gmail.com>
2022-03-16 09:50:28 +0000

    Inserindo PHP ao HTML - Seção 8 - Inclusão de Código

--- Curso_PHP7/secao8/131_PHP_e_HTML/phpHtml.php
+++ Curso_PHP7/secao8/131_PHP_e_HTML/phpHtml.php
@@ -4,3 +4,10 @@
 
 <h1>Seja bem vindo ao nosso site</h1>
 <p><?=$nome ?>, veja as nossas ofertas!</p>
+
+<h2>Confira os nossos principais produtos</h2>
+<ul>
+<?php foreach($produtos as $produto): ?>
+   <li><?=$produto?></li>
+<?php endforeach; ?>
+</ul>
