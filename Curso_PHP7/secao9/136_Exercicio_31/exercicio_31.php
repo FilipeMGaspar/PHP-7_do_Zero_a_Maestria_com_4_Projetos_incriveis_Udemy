9f0ff8b2402739583d2cd28c570e4287610c4b5e
fipewmc <fipewmc2@gmail.com>
2022-03-18 20:07:45 +0000

    Exercício 31- Seção 9 - Funções

--- Curso_PHP7/secao9/136_Exercicio_31/exercicio_31.php
+++ Curso_PHP7/secao9/136_Exercicio_31/exercicio_31.php
@@ -1,2 +1,9 @@
 <?php
-    
+    /*
+    *   Crie um array com strings
+    *   utilize a função implode no array
+    *   Primeiro argumento: ","
+    *   Segundo argumento: o seu array
+    *   Atribua a invocação da função a uma variavel
+    *   Exiba o resultado
+    */
