e0e2e0f4ae9d651fe0ff10c3d572162a945cd94d
fipewmc <fipewmc2@gmail.com>
2022-03-19 20:46:26 +0000

    Exercício 32- Seção 9 - Funções

--- Curso_PHP7/secao9/137_Exercicio_32/Exercicio_32.php
+++ Curso_PHP7/secao9/137_Exercicio_32/Exercicio_32.php
@@ -4,3 +4,9 @@
     *   Define três variaveis numérias dentro dela
     *   Exiba a multicação destes números com echo
     */
+
+    function multiplicar(){
+        $a = 4.5;
+        $b = 2;
+        $c = 1;
+    }
