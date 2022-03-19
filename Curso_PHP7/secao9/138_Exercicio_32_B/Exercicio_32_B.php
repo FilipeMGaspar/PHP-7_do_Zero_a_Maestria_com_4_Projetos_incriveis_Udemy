943e8677d05db972a05960d83707b3e95e454655
fipewmc <fipewmc2@gmail.com>
2022-03-19 21:07:56 +0000

    Exercício 32 B - Seção 9 - Funções

--- Curso_PHP7/secao9/138_Exercicio_32_B/Exercicio_32_B.php
+++ Curso_PHP7/secao9/138_Exercicio_32_B/Exercicio_32_B.php
@@ -4,3 +4,10 @@
 *   Defina uma variavel nome e sobrenome
 *   Imprima o s vlores concatenados
 */
+
+function imprime(){
+    $nome = "Lucas";
+    $sobrenome = "Mendes";
+
+    echo "Ola, $nome $sobrenome. Seja bem Vindo!"
+}
