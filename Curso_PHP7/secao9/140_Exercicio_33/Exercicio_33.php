33dac6c67da0b1cf211bc1005c1edc30679dab99
fipewmc <fipewmc2@gmail.com>
2022-03-20 09:02:38 +0000

    Exercício 33 - Seção 9 - Funções

--- Curso_PHP7/secao9/140_Exercicio_33/Exercicio_33.php
+++ Curso_PHP7/secao9/140_Exercicio_33/Exercicio_33.php
@@ -6,7 +6,12 @@
 */
 
 function apresentarPessoa($nome, $idade){
-    echo "<br>Olá o meu nome é $nome e tenho $idade anos! <br>";
+    if(is_string($nome) && is_int($idade)){
+        echo "<br>Olá o meu nome é $nome e tenho $idade anos! <br>";
+    }else {
+        echo "<br> ERRO [1024] - Nome ou a idade não foram definidos!<br>";
+    }
+    
 }
 
 apresentarPessoa("Julia", 39);
