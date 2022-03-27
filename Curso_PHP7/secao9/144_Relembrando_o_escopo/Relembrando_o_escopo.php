bdc7ef5dc6fab8276324cd6e5dc9da223faa67ec
fipewmc <fipewmc2@gmail.com>
2022-03-27 16:28:36 +0100

    Relembrando o escopo - Seção 9 - Funções

--- Curso_PHP7/secao9/144_Relembrando_o_escopo/Relembrando_o_escopo.php
+++ Curso_PHP7/secao9/144_Relembrando_o_escopo/Relembrando_o_escopo.php
@@ -2,14 +2,19 @@
 
     $a = 10;
 
+    $b = 15;
+
     function testeEscopo(){
         $a = 5;
+        global $b;
 
         $a++;
-        
-        echo "ESCOPO LOCAL DE $a <br><br>";
+
+        echo "ESCOPO LOCAL DE A: $a <br><br>";
+
+        echo "ESCOPO GLOBAL NA FUNÇÃO DE $b <br><br>";
     }
 
-    echo "ESCOPO GLOBAL DE $a <br><br>";
+    echo "ESCOPO GLOBAL DE A: $a <br><br>";
 
     testeEscopo();
