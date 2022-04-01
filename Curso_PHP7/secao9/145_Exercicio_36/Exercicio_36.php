c7fb119ae60155e8851b6abce7fd4e0fe6ac601c
fipewmc <fipewmc2@gmail.com>
2022-04-01 09:44:55 +0100

    Exercício 36 - Seção 9 - Funções

---
+++ Curso_PHP7/secao9/145_Exercicio_36/Exercicio_36.php
@@ -0,0 +1,6 @@
+<?php
+    /*
+    *   Crie uma função que receba um array de números
+    *   Crie um novo array com apenas os números que são maiores que 7
+    *   Retorne este novo arra e imprima na tela
+    */
