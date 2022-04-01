7b016d5e7e566a0a51c84e52687b571d03d8fc67
fipewmc <fipewmc2@gmail.com>
2022-04-01 09:47:15 +0100

    Exercício 36 - Seção 9 - Funções

--- Curso_PHP7/secao9/145_Exercicio_36/Exercicio_36.php
+++ Curso_PHP7/secao9/145_Exercicio_36/Exercicio_36.php
@@ -4,3 +4,10 @@
     *   Crie um novo array com apenas os números que são maiores que 7
     *   Retorne este novo arra e imprima na tela
     */
+
+    function arrayMaoirQueSete ($a = []){
+        print_r($a);
+    }
+
+    $arr = [10, 2 , 8, 16, 5, 3, 6, 9, 25, 125, 1];
+    arrayMaoirQueSete($arr);
