4187b345faf2dadfdbfa9d8ce0628111e362283e
fipewmc <fipewmc2@gmail.com>
2022-04-04 16:01:50 +0100

    Exercício 39 - Seção 10 - Avançando em Strings

---
+++ Curso_PHP7/secao10/156_Exercicio_39/Exercicio_39.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*   Crie uma função que receba características de algum objeto como argumanto (carro, sofa, cafeteira), em array associativo;
+*
+*   O array deve conter nome => preço
+*
+*   retorne apenas os itens que custam mais que R$ 10
+*
+*   Imprima o retorno
+*/ 
