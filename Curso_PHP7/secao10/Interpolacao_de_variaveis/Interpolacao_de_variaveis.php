7ccfb7c2624ff23c737f8bbdfb1bb47bac65985c
fipewmc <fipewmc2@gmail.com>
2022-04-04 15:44:30 +0100

    Interpolação de variáveis - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/Interpolacao_de_variaveis/Interpolacao_de_variaveis.php
+++ Curso_PHP7/secao10/Interpolacao_de_variaveis/Interpolacao_de_variaveis.php
@@ -6,4 +6,4 @@
 
     echo "Olá o meu nome é $nome, tenho $idade anos e trabalho como $profissao<br>";
 
-    echo "Eu tenho {$idade} anos<br>";
+    echo "Olá o meu nome é {$nome}, tenho {$idade} anos e trabalho como {$profissao}<br>";
