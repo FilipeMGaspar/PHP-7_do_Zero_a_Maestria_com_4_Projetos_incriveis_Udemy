2282f991a7561717aff1da5b23e60d8d1e2e4856
fipewmc <fipewmc2@gmail.com>
2022-04-06 09:59:58 +0100

    Comprimento da string - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/159_Comprimento_da_string/Comprimento_da_string.php
+++ Curso_PHP7/secao10/159_Comprimento_da_string/Comprimento_da_string.php
@@ -4,3 +4,12 @@
 
     echo "O nome $nome, tem " . strlen($nome) . " letras <br>";
     echo "Número de Letras = " . strlen($str) . "<br>";
+
+    $len1 = strlen($nome);
+    $len2 = strlen($str);
+
+    if($len1 > $len2){
+        echo "A string '$nome' é maoir que a string '$str' <br>";
+    }else {
+        echo "A string '$str' é maoir que a string '$nome' <br>";
+    }
