c6d981a6f781bba60f1a4b13228d495091c2e217
fipewmc <fipewmc2@gmail.com>
2022-04-06 09:49:33 +0100

    Função printf - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/158_Funcao_printf/Funcao_printf.php
+++ Curso_PHP7/secao10/158_Funcao_printf/Funcao_printf.php
@@ -1,3 +1,12 @@
 <?php
-    printf("Número %d", 1);
-    printf("<br>Salário %f", 856.26);
+
+    // %s -> string
+    $nome = "Filipe";
+    printf("Seja bem vindo, %s", $nome);
+
+    // %d -> int
+    printf("<br>Número %d", 1);
+
+
+    printf("<br>Salário %.2f", 856.26);
+    
