a516bde13737fcf9882d4b5ec18e8978a507fc5a
fipewmc <fipewmc2@gmail.com>
2022-04-06 10:28:08 +0100

    Limpando strings - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/162_Limpando_strings/Limpando_strings.php
+++ Curso_PHP7/secao10/162_Limpando_strings/Limpando_strings.php
@@ -1,1 +1,8 @@
 <?php
+    /*
+    *   trim    limpa espaços antes e depois da string
+    *
+    *   ltrim   limpa espaços da parte inicial da string
+    *
+    *   rtrim   limpa espaços da parte final da string
+    */
