749e80a246601f575b2dac58a0f1775595e7f124
fipewmc <fipewmc2@gmail.com>
2022-04-07 14:29:18 +0100

    Exercício 41 - Seção 10 - Avançando em Strings

---
+++ Curso_PHP7/secao10/165_Exercicio_41/Exercicio_41.php
@@ -0,0 +1,7 @@
+<?php
+    /**
+     * Transforme a string "este item está em promoção"
+     * Em "este item está em Promoção"
+     * Obs: você pode separar as strings, mas não pode escrever em caixa alta ou baixa manualmente, só com funções
+     * 
+     */
