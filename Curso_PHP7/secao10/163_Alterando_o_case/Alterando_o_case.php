3daf5dac1e2813f4f269381ece3c20093cba9be7
fipewmc <fipewmc2@gmail.com>
2022-04-07 14:13:11 +0100

    Alterando o case - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/163_Alterando_o_case/Alterando_o_case.php
+++ Curso_PHP7/secao10/163_Alterando_o_case/Alterando_o_case.php
@@ -2,4 +2,7 @@
     /**
      *      strtolower - todas as letras minúsculas
      *      strtoupper - todas as letras maiúsculas
-     */
+    */
+
+    $str1 = "esta string está em letras minúsculas";
+    $str2 = "";
