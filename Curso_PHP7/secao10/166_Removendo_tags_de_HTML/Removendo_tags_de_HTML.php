e63ff1b231f694a13d5235d83182fe366d530dd8
fipewmc <fipewmc2@gmail.com>
2022-04-07 15:05:21 +0100

    Removendo tags de HTML - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/166_Removendo_tags_de_HTML/Removendo_tags_de_HTML.php
+++ Curso_PHP7/secao10/166_Removendo_tags_de_HTML/Removendo_tags_de_HTML.php
@@ -3,7 +3,7 @@
      *      strip_tags  - Remove as tags html de uma string
      */
 
-     $textoHtml = "<p>textando paragrafo.</p><div> Uma div</div><p>outro paragrafo</p>";
+     $textoHtml = "<p>Testando paragrafo.</p><div> Uma div</div><p>outro paragrafo</p>";
 
     echo $textoHtml . "<br>";
 
