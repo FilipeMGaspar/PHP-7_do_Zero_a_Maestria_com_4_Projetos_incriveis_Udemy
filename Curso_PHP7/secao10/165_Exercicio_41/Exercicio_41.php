00ba1c4cde3da1c886e0df49567521da7ce14595
fipewmc <fipewmc2@gmail.com>
2022-04-07 14:32:21 +0100

    Exercício 41 - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/165_Exercicio_41/Exercicio_41.php
+++ Curso_PHP7/secao10/165_Exercicio_41/Exercicio_41.php
@@ -5,3 +5,7 @@
      * Obs: você pode separar as strings, mas não pode escrever em caixa alta ou baixa manualmente, só com funções
      * 
      */
+
+    header('Content-Type: text/html; charset=UTF-8');
+    
+    $frase = "este item está em promoção";
