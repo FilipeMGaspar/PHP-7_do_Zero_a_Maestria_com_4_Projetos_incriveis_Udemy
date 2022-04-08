d6dc481365ae9525df16dffb270d6018af6483dd
fipewmc <fipewmc2@gmail.com>
2022-04-08 18:14:58 +0100

    Resgatando parte da string - Seção 10 - Avançando em Strings

---
+++ Curso_PHP7/secao10/167_Resgatando_parte_da_string/Resgatando_parte_da_string.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    *   substr(str, inici, fim);
+    *
+    *   str é a string que vamos procurar algo
+    *
+    *   inicio é o indice inicial de palavra ou texto
+    *
+     *  fim é o indice final da palavra ou texto   
+    */
