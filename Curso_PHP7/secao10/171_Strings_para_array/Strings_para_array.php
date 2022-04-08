7384e51a30fe451809f10666ddfca3ca8bfb3ace
fipewmc <fipewmc2@gmail.com>
2022-04-08 19:06:40 +0100

    Repetição de string - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/171_Strings_para_array/Strings_para_array.php
+++ Curso_PHP7/secao10/171_Strings_para_array/Strings_para_array.php
@@ -4,3 +4,11 @@
     $str = "Testando o explode";
 
     $fraseArray = explode(" ", $str);
+
+    print_r($fraseArray);
+    echo "<br><br>";
+
+    $fraseArray2 = explode(",", $str);
+
+    print_r($fraseArray2);
+    echo "<br><br>";
