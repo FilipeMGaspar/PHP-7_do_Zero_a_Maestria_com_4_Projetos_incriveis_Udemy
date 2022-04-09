b788c44616faf7f79940a7b5e897b49c2bf21821
fipewmc <fipewmc2@gmail.com>
2022-04-09 10:59:17 +0100

    Encontrando strings- Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/175_Encontrando_strings/Encontrando_strings.php
+++ Curso_PHP7/secao10/175_Encontrando_strings/Encontrando_strings.php
@@ -4,8 +4,12 @@
  *      strpos    procura texto dentro da string
  */
 
- $str = "estamos testando o método strpos, com strpos podemos emcontrar palavras nas Strings";
+$str = "estamos testando o método strpos, com strpos podemos emcontrar palavras nas Strings";
 
- $testeEncontrar = strpos($str, "strpos");
+$testeEncontrar = strpos($str, "strpos");
 
- echo $testeEncontrar . "<br>";
+echo $testeEncontrar . "<br>";
+
+$testeEncontrar = strpos($str, "melga");
+
+echo $testeEncontrar . "<br>";
