84659915de44e35460d3321dd4550c6484cfc857
fipewmc <fipewmc2@gmail.com>
2022-04-09 10:17:38 +0100

    Exercício 43 - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/172_Exercicio_43/Exercicio_43.php
+++ Curso_PHP7/secao10/172_Exercicio_43/Exercicio_43.php
@@ -11,7 +11,7 @@
      echo $str . "<br><br>";
 
      //print_r($strArray);
-     echo "<h3>Produtos</h3>";
+     echo "<h3>Produtos:</h3>";
      echo "<ul>";   
      foreach ($strArray as $palavra){
         echo "<li>$palavra</li>";
