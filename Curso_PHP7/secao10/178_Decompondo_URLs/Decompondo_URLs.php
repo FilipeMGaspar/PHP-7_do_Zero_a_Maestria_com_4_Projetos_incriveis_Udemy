057986eb6b718e1d18c2e808608d43b3aea0d502
fipewmc <fipewmc2@gmail.com>
2022-04-10 15:02:39 +0100

    Decompondo URLs - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/178_Decompondo_URLs/Decompondo_URLs.php
+++ Curso_PHP7/secao10/178_Decompondo_URLs/Decompondo_URLs.php
@@ -2,12 +2,14 @@
     // parse_url
 
     $url = "https://www.google.com";
-
     $arrayUrl = parse_url($url);
-
     print_r($arrayUrl);
     echo "<br><br>";
 
     echo $arrayUrl["host"] . "<br><br>";
 
     $url = "http://www.horadecodar.com.pt";
+    $arrayUrl = parse_url($url);
+    print_r($arrayUrl);
+    echo "<br><br>";
+    echo $arrayUrl["host"] . "<br><br>";
