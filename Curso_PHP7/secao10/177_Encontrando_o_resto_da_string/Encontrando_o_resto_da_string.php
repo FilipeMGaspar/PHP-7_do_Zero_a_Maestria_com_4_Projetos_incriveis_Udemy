34f17697e1ad433faf4aa71f258b459fedea4fb0
fipewmc <fipewmc2@gmail.com>
2022-04-10 14:55:40 +0100

    Encontrando o resto da string - Seção 10 - Avançando em Strings

--- Curso_PHP7/secao10/177_Encontrando_o_resto_da_string/Encontrando_o_resto_da_string.php
+++ Curso_PHP7/secao10/177_Encontrando_o_resto_da_string/Encontrando_o_resto_da_string.php
@@ -1,8 +1,13 @@
 <?php
     //   strstr
 
-    $str = "testando o resto da String, para ver se dá certo";
+    $str = "testando o resto da string, para ver se dá certo";
 
     $resto = strstr($str, "resto");
+
+    echo $resto . "<br>";
+
+    $s = "string";
+    $resto = strstr($str, $s);
 
     echo $resto . "<br>";
