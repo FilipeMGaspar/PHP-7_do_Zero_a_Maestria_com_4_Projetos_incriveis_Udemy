a14a7dbd880ef295a69ea1c666e611cd42733383
fipewmc <fipewmc2@gmail.com>
2022-04-17 18:51:09 +0100

    Exercício 45 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/184_Exercicio_45/Exercicio_45.php
+++ Curso_PHP7/secao11/184_Exercicio_45/Exercicio_45.php
@@ -1,4 +1,6 @@
 <?php
     /**
-     * Crie um array com a 
+     * Crie um array com a função range de 10 a 45
+     * Imprima todos os números com uma soma de 6
+     * Se a soma passar de 30, imprima também que o número é muito alto
      */
