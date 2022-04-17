e60e79d69faca30dbfda9901e0a5901295656226
fipewmc <fipewmc2@gmail.com>
2022-04-17 18:46:21 +0100

    Criando array rapidamente - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/183_Criando_array_rapidamente/Criando_array_rapidamente.php
+++ Curso_PHP7/secao11/183_Criando_array_rapidamente/Criando_array_rapidamente.php
@@ -1,10 +1,12 @@
 <?php
     // range   // range(1,10)
 
-    $arr = range(1, 10);
+    $arr = range(1, 10); // array de 1 a 10 passo de 
     print_r($arr);
     echo "<br>";
 
-    $arr2 = range(100, 90);
+    $arr2 = range(100, 90); // array de 100 a 90 com passo de 1
     print_r($arr2);
     echo "<br>";
+
+    $arr3 = range(1, 100, 10);//a rray de 100 a 90 com passo de 10
