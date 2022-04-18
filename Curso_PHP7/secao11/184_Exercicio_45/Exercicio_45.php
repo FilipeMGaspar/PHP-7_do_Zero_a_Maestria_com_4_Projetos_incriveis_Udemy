35aa7feb6508e9d029133b025ee1aa4f820acf34
fipewmc <fipewmc2@gmail.com>
2022-04-18 15:00:30 +0100

    Exercício 45 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/184_Exercicio_45/Exercicio_45.php
+++ Curso_PHP7/secao11/184_Exercicio_45/Exercicio_45.php
@@ -9,6 +9,10 @@
     $soma = 0;
     
     for($i = 0; $i <count($arr); $i++){
+        if($arr[$i] + 6 > 30){
+            echo $arr[$i] + 6 . "<br>";
+        } else{
+
+        }
         
-        echo "$arr[i] + 6";
     }
