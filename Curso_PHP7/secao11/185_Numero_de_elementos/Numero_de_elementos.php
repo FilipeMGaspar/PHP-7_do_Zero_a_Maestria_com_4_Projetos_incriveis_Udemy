871cd9e8c453a72a563af2e739b2f0d4c895bcb6
fipewmc <fipewmc2@gmail.com>
2022-04-19 09:42:17 +0100

    Número de elementos - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/185_Numero_de_elementos/Numero_de_elementos.php
+++ Curso_PHP7/secao11/185_Numero_de_elementos/Numero_de_elementos.php
@@ -2,9 +2,13 @@
     // count
 
     $arr = [1, 2, 3];
-
     echo count($arr) . "<br>";
 
     $arr2 = range(1 , 10);
-
     echo count($arr2) . "<br>";
+
+    $arr3 = [
+        "nome" -> "Filipe",
+        "idade" -> 37,
+        "profissao" -> "Sapateiro"
+    ];
