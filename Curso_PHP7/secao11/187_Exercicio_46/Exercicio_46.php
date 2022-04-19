1a42c93f548fd3fdf6233503391a586499992b98
fipewmc <fipewmc2@gmail.com>
2022-04-19 10:02:54 +0100

    Exercício 46 - Seção 11 - Avançando em Arrays

---
+++ Curso_PHP7/secao11/187_Exercicio_46/Exercicio_46.php
@@ -0,0 +1,6 @@
+<?php
+    /**
+     *  Crie um array multidimensional com 3 arrays que tem 4 elementos cada
+     *  Imprima todos os elementos de cada um dos array
+     *  Imprima também quando está mudando de array
+     */
