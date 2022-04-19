7fd7a57405f1d9f48c29de8907383748bf19b704
fipewmc <fipewmc2@gmail.com>
2022-04-19 09:57:34 +0100

    Array multidimensional - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/186_Array_multidimensional/Array_multidimensional.php
+++ Curso_PHP7/secao11/186_Array_multidimensional/Array_multidimensional.php
@@ -7,4 +7,6 @@
     print_r($arr);
     echo "<br>";
 
-    echo $arr[0][1] . "<br>"; // Acessando o primeiro arryy, e segundo elemento  
+    echo $arr[0][1] . "<br>"; // Acessando o primeiro arryy, e segundo elemento
+    
+    echo $arr[1][2] . "<br>"; // Acessando o segundo array, e ultimo elemento
