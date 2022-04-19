961249d1cb0177687f8bb4794cd35f19b20d924d
fipewmc <fipewmc2@gmail.com>
2022-04-19 10:04:20 +0100

    Exercício 46 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/187_Exercicio_46/Exercicio_46.php
+++ Curso_PHP7/secao11/187_Exercicio_46/Exercicio_46.php
@@ -4,3 +4,10 @@
      *  Imprima todos os elementos de cada um dos array
      *  Imprima também quando está mudando de array
      */
+
+    $arr = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ];
