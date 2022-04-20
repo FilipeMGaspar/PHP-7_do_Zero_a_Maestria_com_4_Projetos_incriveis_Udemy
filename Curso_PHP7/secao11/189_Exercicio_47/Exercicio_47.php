b58ca809f09b3a76dc231a1c08d31d956caec16e
fipewmc <fipewmc2@gmail.com>
2022-04-20 16:05:02 +0100

    Exercício 47 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/189_Exercicio_47/Exercicio_47.php
+++ Curso_PHP7/secao11/189_Exercicio_47/Exercicio_47.php
@@ -2,4 +2,8 @@
     /**
      *  Crie um array com os seguintes valores :
      *              Jaguar, 3.0, azul, 18, Teto solar, automático
+     * 
+     *  Chame este array de carro
+     *  
+     *  Crie variáveis com base neste array
      */
