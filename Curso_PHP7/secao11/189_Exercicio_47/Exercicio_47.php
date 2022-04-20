3e3d810ab7d31837cfb6d3fd0d664dbf1e9210ba
fipewmc <fipewmc2@gmail.com>
2022-04-20 16:10:13 +0100

    Exercício 47 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/189_Exercicio_47/Exercicio_47.php
+++ Curso_PHP7/secao11/189_Exercicio_47/Exercicio_47.php
@@ -10,4 +10,11 @@
 
      $carro = ["Jaguar", 3.0, "Azul", 18, "Teto de abrir", "Aucomática"];
 
-    list($marca, $celindrada, $cor, $jante, $teto, $caixa) = $carro;
+    list($marca, $celindrada, $cor, $jantes, $teto, $caixa) = $carro;
+
+    echo "$marca <br>";
+    echo "$celindrada <br>";
+    echo "$cor <br>";
+    echo "$jantes <br>";
+    echo "$teto <br>";
+    echo "$caixa <br>";
