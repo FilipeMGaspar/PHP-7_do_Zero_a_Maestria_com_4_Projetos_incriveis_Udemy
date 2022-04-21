7d0d88937ee3497f73ba6c6b1f8dd9fe2003e291
fipewmc <fipewmc2@gmail.com>
2022-04-21 10:23:39 +0100

    Chaves e valores - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/192_Chaves_e_valores/Chaves_e_valores.php
+++ Curso_PHP7/secao11/192_Chaves_e_valores/Chaves_e_valores.php
@@ -2,3 +2,11 @@
     //array_key   -> cria um array apenas com as chaves de um array
 
     //array_values  -> cria um array apenas com os valores de um array
+
+    $carro = [
+        "marca" => "Peugeot",
+        "motor" => "2.0",
+        "teto_solar" => true,
+        "caixa" => "Manual",
+        "portas" => 4
+    ];
