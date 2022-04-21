766e946390ff66414a2946ebf19e1ae3be521cec
fipewmc <fipewmc2@gmail.com>
2022-04-21 10:15:49 +0100

    Dividindo arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/191_Dividindo_arrays/Dividindo_arrays.php
+++ Curso_PHP7/secao11/191_Dividindo_arrays/Dividindo_arrays.php
@@ -4,6 +4,8 @@
     $arr = range(1, 20);
 
     print_r(array_chunk($arr, 4)); // divide o array principal em arrays com 4 elementos cada
-    echo "<br>";
+    echo "<br><br>";
 
-    $array = array_chunk($arr, 10); // divide o array principal em arrays com 10 elementos cada
+    $arrays = array_chunk($arr, 10); // divide o array principal em arrays com 10 elementos cada
+    print_r($arrays);
+    echo "<br><br>";
