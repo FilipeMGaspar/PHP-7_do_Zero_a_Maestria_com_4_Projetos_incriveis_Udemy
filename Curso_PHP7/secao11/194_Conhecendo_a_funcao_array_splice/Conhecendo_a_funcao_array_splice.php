ebef5f61dc740fdaf9f034508a8be45bd8e6bbc1
fipewmc <fipewmc2@gmail.com>
2022-04-22 14:31:15 +0100

    Conhecendo a função array_splice  - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/194_Conhecendo_a_funcao_array_splice/Conhecendo_a_funcao_array_splice.php
+++ Curso_PHP7/secao11/194_Conhecendo_a_funcao_array_splice/Conhecendo_a_funcao_array_splice.php
@@ -8,6 +8,12 @@
 
     print_r($arr);
     echo "<br><br>";
-
     print_r($removidos);
+    echo "<br><br>";
+
+    $arr2 = [1, 2, 3, 4, 5, 6];
+    $removidos2 = array_splice($arr2, 3);
+    print_r($arr2);
+    echo "<br><br>";
+    print_r($removidos2);
     echo "<br><br>";
