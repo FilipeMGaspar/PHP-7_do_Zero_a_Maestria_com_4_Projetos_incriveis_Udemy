ba35ca24183c396e18e0cfe1e55b631e84e9fa64
fipewmc <fipewmc2@gmail.com>
2022-04-22 14:51:21 +0100

    Utilizando o extract  - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/196_Utilizando_o_extract/Utilizando_o_extract.php
+++ Curso_PHP7/secao11/196_Utilizando_o_extract/Utilizando_o_extract.php
@@ -12,3 +12,12 @@
     echo "$cor <br>";
     echo "$forma <br>";
     echo "$material <br>";
+
+    $nome = "Filipe";
+
+    $pessoa = [
+        "nome" => "Miguel",
+        "idade" => 37
+    ];
+
+    extract($pessoa);
