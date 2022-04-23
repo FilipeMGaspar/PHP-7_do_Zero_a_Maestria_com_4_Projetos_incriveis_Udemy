a00c80ac625de9342f1711394975e8765b7839e6
fipewmc <fipewmc2@gmail.com>
2022-04-23 15:03:12 +0100

    Exercício 50 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/200_Exercicio_50/Exercicio_50.php
+++ Curso_PHP7/secao11/200_Exercicio_50/Exercicio_50.php
@@ -3,4 +3,9 @@
      *  Crie um array associativo com nomes e idades
      *  Imprima estes dados em uma tabela de Html
      *  Dica: utilize as tags do elemento table
-     */
+    */
+
+    $joao{
+        "Nome" => "João",
+        "Idade" => 42   
+    }
