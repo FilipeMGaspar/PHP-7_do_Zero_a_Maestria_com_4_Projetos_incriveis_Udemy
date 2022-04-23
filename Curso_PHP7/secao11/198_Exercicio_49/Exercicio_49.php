2c4065bc9471a8e6e67252a3676392c57dda7d7e
fipewmc <fipewmc2@gmail.com>
2022-04-23 14:28:58 +0100

    Exercício 49 - Seção 11 - Avançando em Arrays

---
+++ Curso_PHP7/secao11/198_Exercicio_49/Exercicio_49.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     *  Crie variaveis com caracteristicas de algum objeto ou animal
+     *  
+     *  Depois crie um array utilizando o método compact com as variaveis criadas na alinie anterior
+     * 
+     * Faça um loop no array e imprima os valores
+     */
