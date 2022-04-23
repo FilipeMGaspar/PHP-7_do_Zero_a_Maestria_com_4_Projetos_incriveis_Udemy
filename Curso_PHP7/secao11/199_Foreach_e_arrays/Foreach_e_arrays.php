c9fe8973f48bce083bd8d75cdb143a8c56620711
fipewmc <fipewmc2@gmail.com>
2022-04-23 14:51:53 +0100

    Foreach e arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/199_Foreach_e_arrays/Foreach_e_arrays.php
+++ Curso_PHP7/secao11/199_Foreach_e_arrays/Foreach_e_arrays.php
@@ -3,4 +3,8 @@
         // notação chave => valor
         // foreach($itens as $key => $value){}
 
-        
+        $pessoa = [
+            "nome" => "Filipe",
+            "idade" => 37,
+            "profissao" => "Sapateiro"
+        ];
