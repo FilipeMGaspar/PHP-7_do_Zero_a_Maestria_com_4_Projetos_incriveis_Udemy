88f0593e16f2a23739697b363d6b5b65253a576b
fipewmc <fipewmc2@gmail.com>
2022-04-23 14:32:23 +0100

    Exercício 49 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/198_Exercicio_49/Exercicio_49.php
+++ Curso_PHP7/secao11/198_Exercicio_49/Exercicio_49.php
@@ -1,8 +1,15 @@
 <?php
     /**
-     *  Crie variaveis com caracteristicas de algum objeto ou animal
-     *  
-     *  Depois crie um array utilizando o método compact com as variaveis criadas na alinie anterior
-     * 
-     * Faça um loop no array e imprima os valores
-     */
+    *  Crie variaveis com caracteristicas de algum objeto ou animal
+    *  
+    *  Depois crie um array utilizando o método compact com as variaveis criadas na alinie anterior
+    * 
+    * Faça um loop no array e imprima os valores
+    */
+
+    $raca = "Pastor alemão";
+    $corPelo = "Castanho";
+    $peso = 65.3;
+    $numPatas = 4;
+
+    $animal = compact("raca", "corPelo", "peso");
