fe7fd59d59cd46a8be481228f64b9cdda9f364b4
fipewmc <fipewmc2@gmail.com>
2022-04-28 15:59:50 +0100

    Buscando em arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/202_Buscando_em_arrays/Buscando_em_arrays.php
+++ Curso_PHP7/secao11/202_Buscando_em_arrays/Buscando_em_arrays.php
@@ -2,26 +2,26 @@
     // in_array     in_array("item", $arr);
 
     $arr = ["Banana", "Maçã", "Batata", "Pera", "Mamão"];
+    
     $busca = "Batata";
-
     if(in_array($busca, $arr)){
         echo "O item <strong>$busca</strong> foi encontrado no array!<br>";
     } else {
         echo "O item <strong>$busca</strong> Não foi encontrado array!<br>";
     }
 
+    echo "<br>";
     $busca = "Banana";
-
     if(in_array($busca, $arr)){
         echo "O item <strong>$busca</strong> foi encontrado no array!<br>";
     } else {
         echo "O item <strong>$busca</strong> Não foi encontrado array!<br>";
     }
 
+    echo "<br>";
     $busca = "Feijão";
-
     if(in_array($busca, $arr)){
         echo "O item <strong>$busca</strong> foi encontrado no array!<br>";
     } else {
-        echo "O item <strong>$busca</strong> Não foi encontrado array!<br>";
+        echo "O item <strong>$busca</strong> não foi encontrado array!<br>";
     }
