7575cc9dbf7ea22da8081a5b2a9760ae48abc78a
fipewmc <fipewmc2@gmail.com>
2022-04-28 15:54:38 +0100

    Buscando em arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/202_Buscando_em_arrays/Buscando_em_arrays.php
+++ Curso_PHP7/secao11/202_Buscando_em_arrays/Buscando_em_arrays.php
@@ -2,3 +2,7 @@
     // in_array     in_array("item", $arr);
 
     $arr = ["Banana", "Maçã", "Batata", "Pera", "Mamão"];
+
+    if(in_array("Batata", $arr)){
+        echo "O item <strong>Batata</strong> encontra-se no array";
+    }
