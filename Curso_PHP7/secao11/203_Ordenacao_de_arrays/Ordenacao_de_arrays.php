d920cbb4f4cb9b9a57aa620e6f8da85fb244ed42
fipewmc <fipewmc2@gmail.com>
2022-04-28 16:06:24 +0100

    Ordenação de arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/203_Ordenacao_de_arrays/Ordenacao_de_arrays.php
+++ Curso_PHP7/secao11/203_Ordenacao_de_arrays/Ordenacao_de_arrays.php
@@ -5,3 +5,9 @@
     // rsort     coloca por ordem decrescente    
 
     $arr = [5, 6, 9, 3, 356, 32, 123, 4, 12, 999, 1, 8];
+    print_r($arr);
+    echo "<br>";
+
+    sort($arr);
+    print_r($arr);
+    echo "<br>";
