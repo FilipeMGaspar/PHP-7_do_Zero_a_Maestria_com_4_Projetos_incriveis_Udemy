86ba5c1f528155e961176ddaa49c5fafebff3072
fipewmc <fipewmc2@gmail.com>
2022-05-03 10:02:59 +0100

    Exercício 51 - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/205_Exercicio_51/Exercicio_51.php
+++ Curso_PHP7/secao11/205_Exercicio_51/Exercicio_51.php
@@ -1,1 +1,6 @@
 <?php
+/**
+ *  Crie um array associativo com chaves com o valor de nomes, e valores com uma pontuação
+ *  Ordene os dados do maior para o menor
+ *  Exiba uma lista, simulando um ranking, em HTML;
+ */
