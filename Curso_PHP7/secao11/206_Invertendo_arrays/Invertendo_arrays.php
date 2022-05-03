e9da2313d0cee01f5028ebea053b463452941b00
fipewmc <fipewmc2@gmail.com>
2022-05-03 10:18:18 +0100

    Invertendo arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/206_Invertendo_arrays/Invertendo_arrays.php
+++ Curso_PHP7/secao11/206_Invertendo_arrays/Invertendo_arrays.php
@@ -5,5 +5,10 @@
 
     $arrRev = array_reverse($arr);
 
+    echo "Array Original: <br>";
     print_r($arr);
+    echo "<br<<br>";
+
+    echo "Array Invertido: <br>";
+    print_r($arrRev);
     echo "<br<<br>";
