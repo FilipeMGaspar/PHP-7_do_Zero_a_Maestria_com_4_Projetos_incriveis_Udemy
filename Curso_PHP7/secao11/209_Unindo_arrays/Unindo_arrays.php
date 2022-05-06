e7a3374e61c858772d6ea494a27c1890c457b0ae
fipewmc <fipewmc2@gmail.com>
2022-05-06 10:03:42 +0100

    Unindo arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/209_Unindo_arrays/Unindo_arrays.php
+++ Curso_PHP7/secao11/209_Unindo_arrays/Unindo_arrays.php
@@ -4,7 +4,7 @@
     $arr =  [1, 2, 3];
     $arr2 = [10, 40, 90];
     $arr3 = [2.3, 44.5, 43.3];
-    $arr4 = ["Maria", "joão", "Nita"]
+    $arr4 = ["abc", "pty", "zxy"];
 
     $arrMerge = array_merge($arr, $arr2, $arr3, $arr4);
 
