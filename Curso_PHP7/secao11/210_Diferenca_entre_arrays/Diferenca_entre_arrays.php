5153fa0fcab338d35ec776492f778c4feafd9356
fipewmc <fipewmc2@gmail.com>
2022-05-06 10:11:59 +0100

    Diferença entre arrays - Seção 11 - Avançando em Arrays

--- Curso_PHP7/secao11/210_Diferenca_entre_arrays/Diferenca_entre_arrays.php
+++ Curso_PHP7/secao11/210_Diferenca_entre_arrays/Diferenca_entre_arrays.php
@@ -4,7 +4,10 @@
     $arr = [1, 2, 3];
     $arr2 = [2, 4 , 6];
 
-    $dif = array_diff($arr, $arr2);
+    $dif = array_diff($arr, $arr2); // Diferença do arr para o arr2
+    print_r($dif);
+    echo "<br><br>";
 
+    $dif = array_diff($arr2, $arr); // Diferença do arr2 para o arr
     print_r($dif);
     echo "<br>";
