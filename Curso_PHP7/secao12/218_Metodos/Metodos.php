2397e8db547d256f04b2ec3a9214f6112bbc2042
fipewmc <fipewmc2@gmail.com>
2022-05-07 12:46:34 +0100

    Métodos - Seção 12 - introdução a Orientação a Objetos

--- Curso_PHP7/secao12/218_Metodos/Metodos.php
+++ Curso_PHP7/secao12/218_Metodos/Metodos.php
@@ -5,11 +5,16 @@
         function falar(){
             echo "Olá, eu sou um objeto <br>";
         }
+
+        function somar($n, $n1){
+            echo $n + $n1 . "<br>";
+        }
     }
 
     $matheu = new Pessoa;
     $matheu->falar();
     $matheu->falar();
+    $matheu->somar(2, 5);
 
     $joao = new Pessoa;
     $joao->falar();
