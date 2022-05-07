c5a2ea2680d40830e5b7d342ade31fb4be221dcc
fipewmc <fipewmc2@gmail.com>
2022-05-07 13:03:56 +0100

    Propriedades - Seção 12 - introdução a Orientação a Objetos

--- Curso_PHP7/secao12/220_Propriedades/Propriedades.php
+++ Curso_PHP7/secao12/220_Propriedades/Propriedades.php
@@ -2,8 +2,10 @@
     class Car{
         public $rodas = 4;
         public $aro = 20;
+        public $cor = "Vermelho";
     }
 
     $ferrari = new Car;
     echo $ferrari->aro . "<br>";
     echo $ferrari->rodas ."<br>";
+    $ferrari->cor="Azul";
