bf179eba5f730028310b2c5442d2744d0768a930
fipewmc <fipewmc2@gmail.com>
2022-05-11 16:55:05 +0100

    Exercício 55 - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/223_Exercicio_55/Exercicio_55.php
+++ Curso_PHP7/secao12/223_Exercicio_55/Exercicio_55.php
@@ -1,2 +1,7 @@
 <?php
-    
+    /**
+     *  Crie uma classe Carro
+     *  Crie algumas propriedades e também a propriedade velocidade_maxima
+     *  Crie o método setVelocidadeMaxima, onde é possivel alterar a velocidade máxima do carro
+     *  e também get VelocidadeMáxima onde é possivel imprimeir a velocidade do carro
+     */
