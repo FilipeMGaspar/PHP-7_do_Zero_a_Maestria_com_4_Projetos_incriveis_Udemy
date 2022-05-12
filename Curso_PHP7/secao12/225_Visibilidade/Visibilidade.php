4228d601e998cb933449be27c12aa7bcf2635521
fipewmc <fipewmc2@gmail.com>
2022-05-12 19:08:31 +0100

    Visibilidade - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/225_Visibilidade/Visibilidade.php
+++ Curso_PHP7/secao12/225_Visibilidade/Visibilidade.php
@@ -3,6 +3,14 @@
         public $rodas = 4;
     }
 
-    $carro = new Car;
+    class Mecanico{
+        public function alterarRodas($obj){
+            $obj->rodas = 10;
+        }
+    }
 
+    $carro = new Car;
     echo $carro->rodas . "<br>";
+
+    $matheus = new Mecanico;
+    $matheus->alterarRodas();
