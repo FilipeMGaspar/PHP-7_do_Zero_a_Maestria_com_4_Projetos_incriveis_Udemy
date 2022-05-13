4bd8944e7cc0934e873695444141d51aa01d9da9
fipewmc <fipewmc2@gmail.com>
2022-05-13 22:49:43 +0100

    Exercício 56 - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/227_Exercicio_56/Exercicio_56.php
+++ Curso_PHP7/secao12/227_Exercicio_56/Exercicio_56.php
@@ -1,2 +1,6 @@
 <?php
-    
+    /**
+     *  Crie uma classe Humano com algumas propriedades e o método falar;
+     *  Crie uma outra classe Professor que herda de humano, crie também as propriedades e métodos particulares desta classe
+     * Exiba os valores das propriedades da classe pais e também utilize os métodos
+     */
