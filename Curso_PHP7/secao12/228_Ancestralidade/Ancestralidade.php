808525b7497e7c5c49510f0d294a9dc2b6d66182
fipewmc <fipewmc2@gmail.com>
2022-05-14 21:09:43 +0100

    Ancestralidade - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/228_Ancestralidade/Ancestralidade.php
+++ Curso_PHP7/secao12/228_Ancestralidade/Ancestralidade.php
@@ -12,3 +12,11 @@
     if($marcos instanceof Humano){
         echo "Marcos é um Humano <br>";
     }
+
+    $turca = new Animal;
+
+    if($turca instanceof Humano){
+        echo "turca é um Humano <br>";
+    } else {
+        echo "turca Não é um Humano <br>";
+    }
