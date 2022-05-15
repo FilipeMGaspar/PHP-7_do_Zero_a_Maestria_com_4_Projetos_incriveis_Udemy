071709356a801d134523022225dbd9e5ce70e2ad
fipewmc <fipewmc2@gmail.com>
2022-05-15 19:06:30 +0100

    Interfaces - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/229_Interfaces/Interfaces.php
+++ Curso_PHP7/secao12/229_Interfaces/Interfaces.php
@@ -1,8 +1,14 @@
 <?php
-    interface Caracteristicas {
+   
+   interface Caracteristicas {
         public function falar();
     }
 
     class Humano implements Caracteristicas {
-        
+        public $idade = 29;
+        public function falar(){
+            echo "Olá, Mundo! <br>";
+        }
     }
+
+    $matheus = new Humano;
