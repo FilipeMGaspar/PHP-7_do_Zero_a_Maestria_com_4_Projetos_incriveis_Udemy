a97a650802e4ab58c420eeea9f745da3363b840d
fipewmc <fipewmc2@gmail.com>
2022-05-16 10:15:39 +0100

    Exercício 57- Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/233_Exercicio_57/Exercicio_57.php
+++ Curso_PHP7/secao12/233_Exercicio_57/Exercicio_57.php
@@ -1,2 +1,6 @@
 <?php
-    
+    /**
+     *  Crie uma classe Cachorro com propriedades
+     *  Inicie as propriedades via construtor;
+     *  Crie um método para exebir cada uma das propriedades que voce vriou
+     */
