289efc5aec201e5ef127c3f3ceb87a14c7995ad9
fipewmc <fipewmc2@gmail.com>
2022-05-16 10:11:46 +0100

    Constructor - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/232_Constructor/Constructor.php
+++ Curso_PHP7/secao12/232_Constructor/Constructor.php
@@ -14,3 +14,6 @@
     $ferrari = new Car(4, "Vermelha", "Ferrari");
 
     echo "O carro é da marca $ferrari->marca e tem a cor $ferrari->cor<br>";
+
+    $bmw = new Car(2, "Cinza", "BMW");
+    echo "É um carro de $bmw->portas portas, da marca $bmw->marca de cor $bmw->cor<br>";
