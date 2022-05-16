bc1b5b93c59b4992d3fd267fa0c258d585f8fd20
fipewmc <fipewmc2@gmail.com>
2022-05-16 10:21:14 +0100

    Exercício 57- Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/233_Exercicio_57/Exercicio_57.php
+++ Curso_PHP7/secao12/233_Exercicio_57/Exercicio_57.php
@@ -5,14 +5,14 @@
      *  Crie um método para exebir cada uma das propriedades que voce vriou
      */
 
-     class Cachorro {
-         private $raca;
-         private $cor;
-         private $idade;
+    class Cachorro {
+        private $raca;
+        private $cor;
+        private $idade;
 
-         public __construct($raca, $cor, $idade){
+        public __construct($raca, $cor, $idade){
             $this->raca = $raca;
             $this->cor = $cor;
             $this->idade = $idade;
-         }
-     }
+        }
+    }
