eb8a50f292f8767d62309f54c99b61bcc6bc0b74
fipewmc <fipewmc2@gmail.com>
2022-05-18 15:52:32 +0100

    Verificando classes - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/235_Verificando_classes/Verificando_classes.php
+++ Curso_PHP7/secao12/235_Verificando_classes/Verificando_classes.php
@@ -7,3 +7,9 @@
     if(class_exists("Humano")){
         echo "A classe existe <br<";
     }
+
+    if(class_exists("Cachorro")){
+        echo "A classe existe <br>";
+    } else {
+        echo "A classe não existe <br>";
+    }
