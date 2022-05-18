dd14710e91859ec8a978a978fec5bf9e0c7b8956
fipewmc <fipewmc2@gmail.com>
2022-05-18 16:02:11 +0100

    Verificando objetos - Seção 12 - Introdução a Orientação a Objetos

--- Curso_PHP7/secao12/236_Verificando_objetos/Verificando_objetos.php
+++ Curso_PHP7/secao12/236_Verificando_objetos/Verificando_objetos.php
@@ -8,7 +8,15 @@
 
     $matheus = new Humano;
 
+    $teste = 10;
+
     if(is_object($matheus)){
+        echo "É um objeto <br>";
+    } else {
+        echo "Não é um objeto <br>";
+    }
+
+    if(is_object($teste)){
         echo "É um objeto <br>";
     } else {
         echo "Não é um objeto <br>";
