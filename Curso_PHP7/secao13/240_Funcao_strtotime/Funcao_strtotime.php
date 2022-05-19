befa28a2d7d50d17f6d213066d794c23df896dab
fipewmc <fipewmc2@gmail.com>
2022-05-19 15:07:15 +0100

    Função strtotime - Seção 13 - Trabalhando com datas

--- Curso_PHP7/secao13/240_Funcao_strtotime/Funcao_strtotime.php
+++ Curso_PHP7/secao13/240_Funcao_strtotime/Funcao_strtotime.php
@@ -2,18 +2,15 @@
 
     $cincoDias = strtotime("5 days");
     echo $cincoDias . "<br>";
-
     $datAtualMais5 = date('d/m/Y', $cincoDias);
     echo $datAtualMais5 . "<br><br>";
 
     $dezDias = strtotime("10 days");
-    echo $dezDias . "<br><br>";
-
+    echo $dezDias . "<br>";
     $datAtualMais10 = date('d/m/Y', $dezDias);
     echo $datAtualMais10 . "<br><br>";
 
     $doisMeses = strtotime("2 months");
     echo $doisMeses . "<br>";
-
     $dataAtualMais2Meses = date('d/m/Y', $doisMeses);
     echo $dataAtualMais2Meses . "<br><br>";
