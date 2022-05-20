71a6665eacbb396670b2057ad4fac757eac371d0
fipewmc <fipewmc2@gmail.com>
2022-05-20 14:22:24 +0100

    Função mktime - Seção 13 - Trabalhando com datas

--- Curso_PHP7/secao13/241_Funcao_mktime/Funcao_mktime.php
+++ Curso_PHP7/secao13/241_Funcao_mktime/Funcao_mktime.php
@@ -1,5 +1,5 @@
 <?php
-    $dataNascimento = mktime(02, 12, 33, 01, 20, 1985);
+    $dataNascimento = mktime(02, 12, 33, 01, 20, 1985); // mktime(Hora, Minuto, Segundo, mês, dia, Ano)
     echo $dataNascimento . "<br>";
 
     $dataNascimentoFormatada = date('d/m/Y', $dataNascimento);
