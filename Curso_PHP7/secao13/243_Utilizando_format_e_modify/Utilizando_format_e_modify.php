7830776e22db984716b34e5be9a3ab1e7e6b530c
fipewmc <fipewmc2@gmail.com>
2022-05-23 21:36:15 +0100

    Utilizando format e modify - Seção 13 - Trabalhando com datas

--- Curso_PHP7/secao13/243_Utilizando_format_e_modify/Utilizando_format_e_modify.php
+++ Curso_PHP7/secao13/243_Utilizando_format_e_modify/Utilizando_format_e_modify.php
@@ -2,12 +2,12 @@
     $data = new DateTime();
 
     //Data Atual
-    echo "";
+    echo "Data atual: <br>";
     echo $data->format('d/m/Y') . "<br>";
     echo $data->format('D - M - Y') . "<br>";
     echo $data->format('l d, F - Y') . "<br>";
 
     //Data atual mais 5 dias
-    echo "<br>Data atual mais 5 dias <br>";
+    echo "<br>Data atual mais 5 dias ..: <br>";
     $data->modify('+5 days');
     echo $data->format('l d, F - Y') . "<br>";
