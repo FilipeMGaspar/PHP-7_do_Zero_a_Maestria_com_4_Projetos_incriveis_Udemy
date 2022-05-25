b6a91b66cf527629325665b9ddff8cb4b313a52f
fipewmc <fipewmc2@gmail.com>
2022-05-25 10:55:56 +0100

    Comparando datas - Seção 13 - Trabalhando com datas

--- Curso_PHP7/secao13/246_Comparando_datas/Comparando_datas.php
+++ Curso_PHP7/secao13/246_Comparando_datas/Comparando_datas.php
@@ -2,6 +2,7 @@
 
     $dataA = new DateTime();
     $dataB = new DateTime();
+    $dataC = new DateTime();
 
     $dataB->setDate(2024, 10, 10);
 
@@ -9,6 +10,12 @@
         echo "A data B é maior que a data A";
     } 
 
+    echo "<br><br>";
+
     if($dataA < $dataB) {
         echo "A data A é menor que a data A";
+    } 
+
+    if($dataA == $dataC) {
+        echo "A data A é igual á data C";
     } 
