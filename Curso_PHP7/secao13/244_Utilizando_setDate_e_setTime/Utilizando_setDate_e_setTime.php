ffaa6f60a7a8ce47a1fc9bb2728ab80f80dde61d
fipewmc <fipewmc2@gmail.com>
2022-05-25 10:30:50 +0100

    Utilizando setDate e setTime - Seção 13 - Trabalhando com datas

--- Curso_PHP7/secao13/244_Utilizando_setDate_e_setTime/Utilizando_setDate_e_setTime.php
+++ Curso_PHP7/secao13/244_Utilizando_setDate_e_setTime/Utilizando_setDate_e_setTime.php
@@ -7,3 +7,7 @@
     $data->setDate(1985, 01, 20);  // Ano, Mes, Dia
     print_r($data);
     echo "<br><br>";
+
+    $data->setTime(05, 12, 10); // Horas, Minutos, Segundos
+    print_r($data);
+    echo "<br><br>";
