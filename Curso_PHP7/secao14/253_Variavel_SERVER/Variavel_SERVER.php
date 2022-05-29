cbd11f3ec4c08de86b0ef3acd9aee8b82ff49840
fipewmc <fipewmc2@gmail.com>
2022-05-29 17:25:33 +0100

    Variável $_SERVER - Seção 14 - PHP e a Web

--- Curso_PHP7/secao14/253_Variavel_SERVER/Variavel_SERVER.php
+++ Curso_PHP7/secao14/253_Variavel_SERVER/Variavel_SERVER.php
@@ -1,2 +1,9 @@
 <?php
     print_r($_SERVER);
+
+    echo "<br><hr><br>";
+    echo $_SERVER['MYSQL_HOME'] . "<br><br>";
+
+    if($_SERVER['SERVER_NAME'] == 'localhost'){
+        echo "Está conectado no " . $_SERVER['SERVER_Name'] . "<br><br>"; 
+    }   
