d459c4dcc4bb110b0c38011dd343d3f28ccd9eb8
fipewmc <fipewmc2@gmail.com>
2022-05-30 14:43:10 +0100

    Formulários com GET  - Seção 14 - PHP e a Web

--- Curso_PHP7/secao14/255_Formularios_com_GET/processamento.php
+++ Curso_PHP7/secao14/255_Formularios_com_GET/processamento.php
@@ -2,9 +2,16 @@
     print_r($_GET);
     echo "<br><br>";
     
-    $nome = $_GET['nome'];
-    $idade = $_GET['idade'];
+    if(isset($_GET['nome']) && isset($_GET['idade'])){
+        $nome = $_GET['nome'];
+        $idade = $_GET['idade'];
+    } else {
+        $nome = "Sem nome";
+        $idade = 0;
+    }
 
     echo "<p>Olá $nome. Você tem $idade anos. Certo?</p>";
 
 ?>
+
+<h3>O seu nome é <?= $nome?>, e tem <?= $idade?> anos</h3>
