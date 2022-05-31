f472ad84aae18ae8d0a553ee2e59bdc4a7f1e350
fipewmc <fipewmc2@gmail.com>
2022-05-31 15:53:21 +0100

    Preenchendo formulários - Seção 14 - PHP e a Web

--- Curso_PHP7/secao14/258_Preenchendo_formularios/Preenchendo_formularios.php
+++ Curso_PHP7/secao14/258_Preenchendo_formularios/Preenchendo_formularios.php
@@ -19,11 +19,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Preenchendo formulários</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="">
         <div>
-            <input type="text" name="nome" placeholder="Digite o seu nome">
+            <input type="text" name="nome" placeholder="Digite o seu nome" value="<?=$nome ?>">
         </div>
     </form>
 </body>
