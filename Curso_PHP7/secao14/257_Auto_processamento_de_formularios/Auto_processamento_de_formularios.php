65e6dec3c587b75a5f34cce36dcad6d19d192180
fipewmc <fipewmc2@gmail.com>
2022-05-31 15:26:21 +0100

    Auto processamento de formulários - Seção 14 - PHP e a Web

--- Curso_PHP7/secao14/257_Auto_processamento_de_formularios/Auto_processamento_de_formularios.php
+++ Curso_PHP7/secao14/257_Auto_processamento_de_formularios/Auto_processamento_de_formularios.php
@@ -1,2 +1,17 @@
 <?php
-    
+    echo $_SERVER['REQUEST_METHOD'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto processamento de formulários</title>
+</head>
+<body>
+    <form action="Auto_processamento_de_formularios.php" method="POST">
+
+    </form>
+</body>
+</html>
