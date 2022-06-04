b1304a2326c3c62e508d56af60985b2a52b18954
fipewmc <fipewmc2@gmail.com>
2022-06-04 10:10:52 +0100

    Separação de templates - Seção 15 - Projeto1 - Blog

--- Curso_PHP7/secao15/270_Separacao_de_templates/templates/footer.php
+++ Curso_PHP7/secao15/270_Separacao_de_templates/templates/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>Hora de codar &copy; 2022</p>
-    <p>Acedidio por <?php $_SERVER['REMOTE_ADDR']. date('d/m/Y'); ?></p>
+    <p>Obrigado por nos visitar, <?php echo $_SERVER['REMOTE_ADDR'] . ", hoje dia " . date('d/m/Y'); ?></p>
 </footer>
 </body>
 </html>
