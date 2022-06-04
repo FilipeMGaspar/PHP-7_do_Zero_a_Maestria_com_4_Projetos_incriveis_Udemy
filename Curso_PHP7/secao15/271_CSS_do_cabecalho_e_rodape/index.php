180bb84f1398a2bb85d497534083deb28b4591dc
fipewmc <fipewmc2@gmail.com>
2022-06-04 10:27:38 +0100

    CSS do cabeçalho e rodapé - Seção 15 - Projeto1 - Blog

---
+++ Curso_PHP7/secao15/271_CSS_do_cabecalho_e_rodape/index.php
@@ -0,0 +1,8 @@
+
+    <?php
+        require_once "templates/header.php";
+    ?>
+    <h1>Olá, Mundo!</h1>
+    <?php
+        require_once "templates/footer.php";
+    ?>
---
+++ Curso_PHP7/secao15/271_CSS_do_cabecalho_e_rodape/templates/header.php
@@ -0,0 +1,31 @@
+<?php
+    include_once "helpers/url.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar!</title>
+    <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">    
+</head>
+<body>
+    <header>
+        <a href="<?= $_BASE_URL; ?>" id="logo">
+            <img src="<?= $_BASE_URL; ?>img/logo.svg" alt="blog codar">
+        </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="<?= $_BASE_URL; ?>">Home</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="<?= $_BASE_URL; ?>contato.php">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>
