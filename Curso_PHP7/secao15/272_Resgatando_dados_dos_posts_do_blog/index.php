f300a666e7a088c08224930ce5a47b26dfc12f13
fipewmc <fipewmc2@gmail.com>
2022-06-05 11:30:28 +0100

    Resgatando dados dos posts do blog - Seção 15 - Projeto1 - Blog

--- Curso_PHP7/secao15/272_Resgatando_dados_dos_posts_do_blog/index.php
+++ Curso_PHP7/secao15/272_Resgatando_dados_dos_posts_do_blog/index.php
@@ -11,8 +11,8 @@
 
             <?php foreach ($posts as $post): ?>
                 <div class="post-box">
-                    <img src="<?php $_BASE_URL; ?>/img/<?= $post['img'] ?>" alt="<?php $post['title'] ?>">
-                    <h2 class="post-title"><?php ?></h2>
+                    <img src="<?= $_BASE_URL; ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                    <h2 class="post-title"><?= $post['title'] ?></h2>
                 </div>
             <?php endforeach; ?>    
         </div>
