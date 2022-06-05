86dc214fd32f44c0af8f2dbd17d631e310655e61
fipewmc <fipewmc2@gmail.com>
2022-06-05 10:49:37 +0100

    Resgatando dados dos posts do blog - Seção 15 - Projeto1 - Blog

--- Curso_PHP7/secao15/272_Resgatando_dados_dos_posts_do_blog/templates/footer.php
+++ Curso_PHP7/secao15/272_Resgatando_dados_dos_posts_do_blog/templates/footer.php
@@ -1,6 +1,5 @@
 <footer>
     <p>Hora de codar &copy; 2022</p>
-    <p>Obrigado por nos visitar, <?php echo $_SERVER['REMOTE_ADDR'] . ", hoje dia " . date('d/m/Y'); ?></p>
 </footer>
 </body>
 </html>
