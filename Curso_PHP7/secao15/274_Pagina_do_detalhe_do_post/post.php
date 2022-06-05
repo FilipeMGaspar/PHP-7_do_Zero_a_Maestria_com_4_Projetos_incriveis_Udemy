a4c08cee979e1667e2775bb73d32c76dd4f2321a
fipewmc <fipewmc2@gmail.com>
2022-06-05 12:13:56 +0100

    Página do detalhe do post - Seção 15 - Projeto1 - Blog

--- Curso_PHP7/secao15/274_Pagina_do_detalhe_do_post/post.php
+++ Curso_PHP7/secao15/274_Pagina_do_detalhe_do_post/post.php
@@ -17,7 +17,7 @@
             <h1 id="main-title"><?= $currentPost['title'] ?></h1>
             <p id="post-description"><?= $currentPost['description'] ?></p>
             <div class='img-container'>
-                <img src="<?= $_BASE_URL; ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                <img src="<?= $_BASE_URL; ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
             </div>
         </div>
     </main>
