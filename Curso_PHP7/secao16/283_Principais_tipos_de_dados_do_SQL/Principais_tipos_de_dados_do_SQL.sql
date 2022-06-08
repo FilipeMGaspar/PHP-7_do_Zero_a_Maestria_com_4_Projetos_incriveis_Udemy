9a23338d502010ff718f8b7cda56d593c03c44d4
fipewmc <fipewmc2@gmail.com>
2022-06-08 18:47:23 +0100

    Principais tipos de dados da SQL - Seção 16 - PHP e Banco de Dados (MySQL)

--- Curso_PHP7/secao16/283_Principais_tipos_de_dados_do_SQL/Principais_tipos_de_dados_do_SQL.sql
+++ Curso_PHP7/secao16/283_Principais_tipos_de_dados_do_SQL/Principais_tipos_de_dados_do_SQL.sql
@@ -1,5 +1,5 @@
-/* VARCHAR */
-/* TEXT */
-/* INT */
-/* BIGINT */
+/* VARCHAR: texto de 0 a 65535 caracteres */
+/* TEXT: texto com no máximo 65535 bytes */
+/* INT: números inteiros */
+/* BIGINT: números inteiros com maior proporção que o INT */
 /* DATE  -> formato YYYY-MM-DD */
