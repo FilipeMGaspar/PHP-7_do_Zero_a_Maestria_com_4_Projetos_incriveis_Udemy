69a5479627854669e224fc75f2e87681e9498988
fipewmc <fipewmc2@gmail.com>
2022-06-12 20:32:17 +0100

    Inserindo dados - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/291_Inserindo_dados/Inserindo_dados.sql
+++ Curso_PHP7/secao16/291_Inserindo_dados/Inserindo_dados.sql
@@ -1,3 +1,5 @@
 INSERT INTO itens(nome, descricao) VALUES ("SmartPhone", "Melga, Melga, Melga, Melga");
 
 INSERT INTO itens(nome, descricao) VALUES ("Teclado", "Melga, Melga, Melga, Melga");
+
+INSERT INTO itens(nome, descricao) VALUES ("Mochila", "Melga, Melga, Melga, Melga");
