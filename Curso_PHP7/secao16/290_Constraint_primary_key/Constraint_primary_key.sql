3e5eb45c352167590000faf18fcc121f80e5c72d
fipewmc <fipewmc2@gmail.com>
2022-06-12 20:17:57 +0100

    Constraint: primary key - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/290_Constraint_primary_key/Constraint_primary_key.sql
+++ Curso_PHP7/secao16/290_Constraint_primary_key/Constraint_primary_key.sql
@@ -4,9 +4,11 @@ CREATE TABLE itens(
 	descricao TEXT	
 );
 
-INSERT INTO itens (nome, descricao) VALUES ("Abajur", "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry... ");
+INSERT INTO itens (nome, descricao) VALUES ("Estojo", "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry... ");
 
 INSERT INTO itens (nome, descricao) VALUES ("Caneta", "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry... ");
+
+INSERT INTO itens (nome, descricao) VALUES ("Tijolo", "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry... ");
 
 INSERT INTO itens (nome, descricao) VALUES ("Caderno", "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry... ");
 
