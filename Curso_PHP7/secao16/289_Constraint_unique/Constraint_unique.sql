4dd1e5751eb86296a7d7407e21c0e500629eef52
fipewmc <fipewmc2@gmail.com>
2022-06-12 20:01:00 +0100

    Constraint: unique - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/289_Constraint_unique/Constraint_unique.sql
+++ Curso_PHP7/secao16/289_Constraint_unique/Constraint_unique.sql
@@ -1,4 +1,8 @@
-CREATE mailmarketing (
+CREATE TABLE mailmarketing (
 	email VARCHAR(255) NOT NULL UNIQUE,
 	nome VARCHAR(255)
 );
+
+INSERT INTO mailmarketing (email, nome) VALUES ("Maria@mail.pt", 'Maria');
+
+INSERT INTO mailmarketing (email, nome) VALUES ("Maria@mail.pt", 'Maria');
