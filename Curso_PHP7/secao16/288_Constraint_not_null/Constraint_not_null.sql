f0d9c6d3512bd0b13f0a9e1ebb15c9b0a9f49668
fipewmc <fipewmc2@gmail.com>
2022-06-12 19:55:18 +0100

    Constraint not null - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/288_Constraint_not_null/Constraint_not_null.sql
+++ Curso_PHP7/secao16/288_Constraint_not_null/Constraint_not_null.sql
@@ -8,3 +8,5 @@ INSERT INTO notnull (nome) VALUES ("Filipe");
 INSERT INTO notnull (nome, idade) VALUES ("Maria", 12);
 
 INSERT INTO notnull (idade) VALUES (37);
+
+SET @@GLOBAL.SQL_MODE = 'STRICT_ALL_TABLES';
