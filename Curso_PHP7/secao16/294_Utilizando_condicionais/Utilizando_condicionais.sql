fa5990b5621545b4de2eb803ebb45b85429c9f6e
fipewmc <fipewmc2@gmail.com>
2022-06-13 14:13:16 +0100

    Utilizando condicionais - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/294_Utilizando_condicionais/Utilizando_condicionais.sql
+++ Curso_PHP7/secao16/294_Utilizando_condicionais/Utilizando_condicionais.sql
@@ -3,3 +3,6 @@ SELECT * FROM itens WHERE id = 2 OR id = 5;
 INSERT INTO itens (nome, descricao) values ("Caneta", "Azul de ponta dina 0.3");
 
 SELECT * FROM itens WHERE nome = "Caneta" AND id = 8; 
+
+
+SELECT * FROM itens WHERE nome = "Caneta" AND id > 6; 
