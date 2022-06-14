d010d205ee1f4de7d6123439f197aeeaa1ca7e9d
fipewmc <fipewmc2@gmail.com>
2022-06-14 09:44:30 +0100

    Criando usuário no banco - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/298_Criando_usuario_no_banco/Criando_usuario_no_banco.sql
+++ Curso_PHP7/secao16/298_Criando_usuario_no_banco/Criando_usuario_no_banco.sql
@@ -1,6 +1,7 @@
 SELECT * FROM mysql.user;
 
-CREATE USER 'usuario'@'localhost' IDENTIFIED BY '123' GRANT ALL PRIVILEGES ON *.* 'usuario'@'localhost';
-FLUSH PRIVILEGES;
-
 CREATE USER 'filipe'@'localhost' IDENTIFIED BY 'teste123';
+
+GRANT ALL PRIVILEGES ON *.* 'filipe'@'localhost';
+
+FLUSH PRIVILEGES;
