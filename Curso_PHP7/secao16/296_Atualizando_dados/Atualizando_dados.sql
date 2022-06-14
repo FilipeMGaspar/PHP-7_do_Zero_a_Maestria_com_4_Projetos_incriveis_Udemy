ecbdf9f55d0b4157fa31362c9f63e3d46fd10efe
fipewmc <fipewmc2@gmail.com>
2022-06-14 09:24:13 +0100

    Atualizando dados - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/296_Atualizando_dados/Atualizando_dados.sql
+++ Curso_PHP7/secao16/296_Atualizando_dados/Atualizando_dados.sql
@@ -3,3 +3,7 @@ UPDATE mailmarketing set nome = "Josias";
 UPDATE itens SET nome = "Caneta standard" WHERE id = 8;
 
 UPDATE mailmarketing set nome = "Maria" WHERE id = 1;
+
+UPDATE mailmarketing set nome = "Manuel" WHERE id = 3;
+
+UPDATE itens SET nome  = "Caneta Azul 0.7", descricao = "Caneta Azul de ponta 0.7" WHERE id = 8;
