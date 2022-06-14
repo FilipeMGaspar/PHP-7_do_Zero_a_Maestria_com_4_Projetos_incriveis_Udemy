8920af518066f6088218023fc8f4555ce05e2177
fipewmc <fipewmc2@gmail.com>
2022-06-14 09:29:37 +0100

    Deletando dados - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/296_Atualizando_dados/Atualizando_dados.sql
+++ Curso_PHP7/secao16/296_Atualizando_dados/Atualizando_dados.sql
@@ -2,8 +2,8 @@ UPDATE mailmarketing set nome = "Josias";
 
 UPDATE itens SET nome = "Caneta standard" WHERE id = 8;
 
-UPDATE mailmarketing set nome = "Maria" WHERE id = 1;
+UPDATE mailmarketing set nome = "Maria" WHERE email = "Maria@mail.pt";
 
-UPDATE mailmarketing set nome = "Manuel" WHERE id = 3;
+UPDATE mailmarketing set nome = "Ninita" WHERE email = "Nita@mail.pt";
 
 UPDATE itens SET nome  = "Caneta Azul 0.7", descricao = "Caneta Azul de ponta 0.7" WHERE id = 8;
