411382e0926b83204d481f0bfd38d0b88ea2e01a
fipewmc <fipewmc2@gmail.com>
2022-06-14 10:09:16 +0100

    Conectando com mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/300_Conectando_com_mysqli/Conectando_com_mysqli.php
+++ Curso_PHP7/secao16/300_Conectando_com_mysqli/Conectando_com_mysqli.php
@@ -1,4 +1,4 @@
 <?php
     // $conn = new mysqli("host", "user", "password", "db");
 
-    $conn = new mysqli("localhost", "filipe", "teste123", "cursophp");
+    $conn = new mysqli("localhost", "filipe", "aeste123", "cursophp");
