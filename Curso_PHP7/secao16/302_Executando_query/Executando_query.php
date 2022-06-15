63bdd68e55b2a7b03d4bb7891377e2d43c5ee83c
fipewmc <fipewmc2@gmail.com>
2022-06-15 12:18:33 +0100

    Executando query - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/302_Executando_query/Executando_query.php
+++ Curso_PHP7/secao16/302_Executando_query/Executando_query.php
@@ -4,11 +4,11 @@
     $pass = "teste123";
     $db = "cursophp";
 
-    $conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db); // conexão/ligação á base de dados
 
-    $sqlQuery = "SELECT * From itens";
+    $sqlQuery = "SELECT * From itens"; // Query / busca a ser efectuada na base da dados na tabela itens
 
-    $resultado = $conn->query($sqlQuery);
+    $resultado = $conn->query($sqlQuery); // Execução da query/Busca na base de dados
 
     print_r($resultado);
 
