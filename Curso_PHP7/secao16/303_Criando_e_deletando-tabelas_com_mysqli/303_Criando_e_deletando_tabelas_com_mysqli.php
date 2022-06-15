13dc3dce19a151311efa187a6e3cf2865f549013
fipewmc <fipewmc2@gmail.com>
2022-06-15 12:24:34 +0100

    Criando e deletando tabelas com mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/303_Criando_e_deletando-tabelas_com_mysqli/303_Criando_e_deletando_tabelas_com_mysqli.php
+++ Curso_PHP7/secao16/303_Criando_e_deletando-tabelas_com_mysqli/303_Criando_e_deletando_tabelas_com_mysqli.php
@@ -6,6 +6,6 @@
 
     $conn = new mysqli($host, $user, $pass, $db); // Ligação á base de dados
 
-    $sqlQuery = "CREATE TABLE teste (nome VARCHAR(100), sobrenome VARCHAR(100))";
+    $sqlQuery = "CREATE TABLE teste (nome VARCHAR(100), sobrenome VARCHAR(100))"; // Criação da tabela teste com as colunas nome e sobrenome
 
     $conn->close(); // Fecha a ligação á base de dados
