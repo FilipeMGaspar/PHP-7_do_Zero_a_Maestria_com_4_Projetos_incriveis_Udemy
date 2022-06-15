44bc79b0e5b4894824f1524ae3b23ca366c7ef91
fipewmc <fipewmc2@gmail.com>
2022-06-15 12:10:29 +0100

    Erro na conexão - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/301_Erro_na_conexao/erro_na_conexao.php
+++ Curso_PHP7/secao16/301_Erro_na_conexao/erro_na_conexao.php
@@ -7,6 +7,6 @@
     $conn = new mysqli($host, $user, $pass, $db);
 
     if($conn->connect_errno){
-        echo "Erro na conexão";
+        echo "Erro na conexão! <br>";
         echo "Erro: " . mysqli_connect_error();
     }
