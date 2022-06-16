db35686fe52975cf627415b4477ceaa8331b6215
fipewmc <fipewmc2@gmail.com>
2022-06-16 11:42:08 +0100

    Inserindo dados com mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/304_Inserindo_dados_com_mysqli/Inserindo_dados_com_mysqli.php
+++ Curso_PHP7/secao16/304_Inserindo_dados_com_mysqli/Inserindo_dados_com_mysqli.php
@@ -4,15 +4,17 @@
     $pass = "dGVzdGUxMjM=";
     $db = "cursophp";
 
-    $conn = new mysqli($host, $user, base64_decode($pass), $db);
+    $conn = new mysqli($host, $user, base64_decode($pass), $db); // Ligação á base de dados
 
     $tabela = "itens";
     $nome = "Recarga A4 100g Linhas";
     $descricao = "Recarga de folhas de linhas, A4 pautada, 100g 7mm entre linhas";
 
     //Inserir dados noma tabela
-    $query = "INSERT INTO $tabela (nome, descricao) VALUES ($nome, $descricao)";
+    $query = "INSERT INTO $tabela (nome, descricao) VALUES ('$nome', '$descricao')";
 
    // $query = "INSERT INTO itens (nome, descricao) Values ('Caneta verde 0.5', 'Caneta verde 0.5 de ponta fina bic')";
+
+   $conn->query($query); // Executando a query de inserção de dados
 
     $conn->close();
