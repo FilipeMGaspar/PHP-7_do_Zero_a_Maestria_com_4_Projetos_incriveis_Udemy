a527a893c892e4d8785e08815349551de42dfc05
fipewmc <fipewmc2@gmail.com>
2022-06-17 12:58:48 +0100

    Inserindo dados com prepared statements e mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/307_Inserindo_dados_com_prepared_statements_e_mysqli/Inserindo_dados_com_prepared_statements_e_mysqli.php
+++ Curso_PHP7/secao16/307_Inserindo_dados_com_prepared_statements_e_mysqli/Inserindo_dados_com_prepared_statements_e_mysqli.php
@@ -5,3 +5,15 @@
     $db = "cursophp";
 
     $conn = new mysqli($host, $user, $pass, $db);
+
+    $nome = "Suporte de Microfone";
+    $descricao = "O suporte é novo e foi fabricado na china";
+
+    // prepared statements
+    $stmt = $conn->prepare("INSERT INTO itens (nome, descricao) VALUES (?, ?)"); //  ? ? é o bindparam 
+
+    $stmt->bind_param("ss", $nome, $descricao); // "ss" String String, vai subtituir os ? ? da linha anterior  s = String, i = integer, d = double
+
+    $stmt->execute(); // Execução 
+
+    $conn->close();
