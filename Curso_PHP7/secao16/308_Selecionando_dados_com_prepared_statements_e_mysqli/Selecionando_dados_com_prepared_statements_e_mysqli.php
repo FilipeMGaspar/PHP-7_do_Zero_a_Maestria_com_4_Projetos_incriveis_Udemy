0b3152c398af2545834506b3c0e40277739ee88d
fipewmc <fipewmc2@gmail.com>
2022-06-17 13:06:12 +0100

    Selecionando dados com prepared statements e mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/308_Selecionando_dados_com_prepared_statements_e_mysqli/Selecionando_dados_com_prepared_statements_e_mysqli.php
+++ Curso_PHP7/secao16/308_Selecionando_dados_com_prepared_statements_e_mysqli/Selecionando_dados_com_prepared_statements_e_mysqli.php
@@ -6,6 +6,18 @@
 
     $conn = new mysqli($host, $user, $pass, $db);
 
-    
+    $id = 4;
+
+    $stmt = $conn->prepare("SELECT * FROM itens WHERE id > ?");
+
+    $stmt->bin_param("i", $id); // subtitui o ? pelo valor da variavel $id
+
+    $stmt->execute(); // Execução dq query
+
+    $resultado = $stmt->get_result(); //
+
+    $data = $resultado->fetch_all();
 
     $conn->close();
+
+    print_r($data);
