ff73418e56cdb6dc85c9c34ab7829897aedd87f4
fipewmc <fipewmc2@gmail.com>
2022-06-17 14:19:14 +0100

    Atualizando dados com prepared statements e mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/310_Atualizando_dados_com_prepared_statements_e_mysqli/Atualizando_dados_com_prepared_statements_e_mysqli.php
+++ Curso_PHP7/secao16/310_Atualizando_dados_com_prepared_statements_e_mysqli/Atualizando_dados_com_prepared_statements_e_mysqli.php
@@ -6,6 +6,12 @@
 
     $conn = new mysqli($host, $user, $pass, $db);
 
-    
+    $id = 11;
+
+    $stmt = $conn->prepare("SELECT * FROM itens WHERE id = ?");
+
+    $stmt->bind_param("i", $id);
+
+    $stmt->execute();
 
     $conn->close();
