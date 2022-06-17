0c24a3709844e65bbd78b04b4f33fe121397df4a
fipewmc <fipewmc2@gmail.com>
2022-06-17 14:05:24 +0100

    Resgatando apenas um resultado com mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/309_Resgatando_apenas_um_resultado_com_mysqli/Resgatando_apenas_um_resultado_com_mysqli.php
+++ Curso_PHP7/secao16/309_Resgatando_apenas_um_resultado_com_mysqli/Resgatando_apenas_um_resultado_com_mysqli.php
@@ -6,6 +6,18 @@
 
     $conn = new mysqli($host, $user, $pass, $db);
 
-    
+    $id = 4;
+
+    $stmt = $conn->prepare("SELECT * FROM itens WHERE id > ?");
+
+    $stmt->bind_param("i", $id);
+
+    $stmt->execute();
+
+    $resultado = $stmt->get_result();
+
+    $data = $resultado->fetch_row();
 
     $conn->close();
+
+    print_r($data);
