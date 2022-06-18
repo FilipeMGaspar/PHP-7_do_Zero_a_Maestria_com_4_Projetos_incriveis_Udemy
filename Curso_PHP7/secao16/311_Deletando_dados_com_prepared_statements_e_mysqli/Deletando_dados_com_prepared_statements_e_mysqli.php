d78766d993a0ab3016790704ebf8cb3fd2fc5cec
fipewmc <fipewmc2@gmail.com>
2022-06-18 12:57:46 +0100

    Deletando dados com prepared statements e mysqli - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/311_Deletando_dados_com_prepared_statements_e_mysqli/Deletando_dados_com_prepared_statements_e_mysqli.php
+++ Curso_PHP7/secao16/311_Deletando_dados_com_prepared_statements_e_mysqli/Deletando_dados_com_prepared_statements_e_mysqli.php
@@ -6,5 +6,12 @@
 
     $conn = new mysqli ($host, $user, $pass, $db);
 
+    $id = 4;
+
+    $stmt = $conn->prepare("DELETE FROM intem WHERE id = ?");
+
+    $stmt->bind_param("i", $id);
+
+    
 
     $conn->close();
