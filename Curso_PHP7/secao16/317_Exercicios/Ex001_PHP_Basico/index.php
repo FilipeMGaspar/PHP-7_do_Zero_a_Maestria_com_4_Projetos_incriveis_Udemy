3ef6b7ee3e34a4550d591efea00b65cffc5623fc
fipewmc <fipewmc2@gmail.com>
2022-06-18 16:27:24 +0100

    Exercícios PHP Básico - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/317_Exercicios/Ex001_PHP_Basico/index.php
+++ Curso_PHP7/secao16/317_Exercicios/Ex001_PHP_Basico/index.php
@@ -1,3 +1,13 @@
 <?php
     
     echo "Olá, Mundo!";
+
+    /* Extenções para o VsCode para PHP */
+
+    /*
+    *   Utilizando o Xampp temos de adicionar as variaveis de ambiente
+
+    *   code runner
+
+    *   DotENV
+    */
