49634f1476f3c8b9d17cfc08fec12f5f2154f0ac
fipewmc <fipewmc2@gmail.com>
2022-06-19 12:02:42 +0100

    Inserindo dados com PDO - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/315_Atualizando_dados_com_PDO/Atualizando_dados_com_PDO.php
+++ Curso_PHP7/secao16/315_Atualizando_dados_com_PDO/Atualizando_dados_com_PDO.php
@@ -4,19 +4,17 @@
     $user = "root";
     $pass = "";
 
-    $conn = new PDO("mysql:host0$host;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 
+    
     $id = 5;
     $nome = "Readmi";
     $descricao = "Readmi Note 12 Pro, novo em caixa, com todos os acessórios";
 
+    $stmt = $conn->prepare("UPDATE itens SET nome = :nome, descricao = :descricao WHERE id = :id");
 
-   // $stmt = $conn->prepare("UPDATE itens SET nome=:nome, descricao=:descricao WHERE id=:id");
-
-   $stmt = $conn->prepare("UPDATE itens SET nome = :nome, descricao = :descricao WHERE id = :id");
-
+    $stmt->bindParam(":id", $id);
     $stmt->bindParam(":nome", $nome);
     $stmt->bindParam(":descricao", $descricao);
-    $stmt->bindParam(":id", $id);
     
     $stmt->execute();
