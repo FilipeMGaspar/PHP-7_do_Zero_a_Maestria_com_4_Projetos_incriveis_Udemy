abe3100391076210330ad59c2f1a68e056138fc4
fipewmc <fipewmc2@gmail.com>
2022-06-19 11:34:39 +0100

    Inserindo dados com PDO - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/314_Inserindo_dados_com_PDO/Inserindo_dados_com_PDO.php
+++ Curso_PHP7/secao16/314_Inserindo_dados_com_PDO/Inserindo_dados_com_PDO.php
@@ -6,4 +6,12 @@
     
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 
-    
+    $stmt = $conn->prepare("INSERT INTO itens (nome, descricao) VALUES (:nome, :descricao)");
+
+    $nome = "Cadeira Gamer";
+    $descricao = "Cadeira gamer usada, fabricada na china";
+
+    $stmt->bindParam(":nome", $nome);
+    $stmt->bindParam(":descricao", $descricao);
+
+    $stmt->execute();
