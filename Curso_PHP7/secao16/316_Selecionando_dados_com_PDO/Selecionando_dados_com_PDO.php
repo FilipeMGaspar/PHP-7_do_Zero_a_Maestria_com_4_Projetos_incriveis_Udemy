91817455d2babbfbddf65df65014b6ad89a37eae
fipewmc <fipewmc2@gmail.com>
2022-06-19 12:10:34 +0100

    Selecionando dados com PDO - Seção 16 - PHP e Banco de Dados

--- Curso_PHP7/secao16/316_Selecionando_dados_com_PDO/Selecionando_dados_com_PDO.php
+++ Curso_PHP7/secao16/316_Selecionando_dados_com_PDO/Selecionando_dados_com_PDO.php
@@ -5,3 +5,10 @@
     $pass = "teste123";
 
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    $id = 5;
+
+    $stmt = $conn->prepare("SELECT * FROM itens WHERE id > :id");
+
+    $stmt->bindParam(":id", $id);
+    
