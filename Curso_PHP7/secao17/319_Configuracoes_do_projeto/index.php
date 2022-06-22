45ad85fec496626b03df30068e0d665e9846b732
fipewmc <fipewmc2@gmail.com>
2022-06-22 20:33:44 +0100

    Configurações do projeto - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/319_Configuracoes_do_projeto/index.php
+++ Curso_PHP7/secao17/319_Configuracoes_do_projeto/index.php
@@ -1,10 +1,15 @@
+<?php
+    require_once "config/url.php";
+
+    echo "$BASE_URL";
+?>
 <!DOCTYPE html>
 <html lang="pt-pt">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda - Segundo projeto - Matheus Battisti - Hora de Codar</title>
+    <title>Agenda - Segundo projeto PHP - Matheus Battisti - Hora de Codar</title>
 </head>
 <body>
     
