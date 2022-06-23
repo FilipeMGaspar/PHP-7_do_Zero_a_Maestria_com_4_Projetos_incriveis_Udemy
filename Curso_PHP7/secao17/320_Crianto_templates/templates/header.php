0346509fae10d632709b8a9597956550992cfb7b
fipewmc <fipewmc2@gmail.com>
2022-06-23 09:52:10 +0100

    Crianto templates - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/320_Crianto_templates/templates/header.php
+++ Curso_PHP7/secao17/320_Crianto_templates/templates/header.php
@@ -24,5 +24,11 @@
             <a href="<?= $BASE_URL ?>index.php" class="navbar-brand">
                 <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
             </a>
+            <div>
+                <div class="navbar-nav">
+                    <a href="<?= $BASE_URL ?>index.php" class="nav-link active" id="home-link">Agenda</a>
+                    <a href="<?= $BASE_URL ?>create.php" class="nav-link active" id="home-link">Adicionar Contato</a>
+                </div>
+            </div>
         </nav>
     </header>
