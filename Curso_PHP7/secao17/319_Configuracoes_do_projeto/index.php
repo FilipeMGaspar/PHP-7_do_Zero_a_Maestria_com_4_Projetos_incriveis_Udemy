1ecacbeebdfd4bb125577f83eeaa3963f87d5515
fipewmc <fipewmc2@gmail.com>
2022-06-23 09:27:19 +0100

    Crianto templates - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/319_Configuracoes_do_projeto/index.php
+++ Curso_PHP7/secao17/319_Configuracoes_do_projeto/index.php
@@ -1,25 +1,4 @@
-<?php
-    require_once "config/url.php";
 
-    echo "$BASE_URL";
-?>
-<!DOCTYPE html>
-<html lang="pt-pt">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda - Segundo projeto PHP - Matheus Battisti - Hora de Codar</title>
-
-    <!-- Bootstrap ligação á bibliteca por css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.min.css" integrity="sha512-o/MhoRPVLExxZjCFVBsm17Pkztkzmh7Dp8k7/3JrtNCHh0AQ489kwpfA3dPSHzKDe8YCuEhxXq3Y71eb/o6amg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- font-awesome ligação á biblioteca por css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- CSS do projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-</head>
 <body>
     <h1>Primeiro teste na agenda</h1>
     <i class="fa-solid fa-caravan"></i>
