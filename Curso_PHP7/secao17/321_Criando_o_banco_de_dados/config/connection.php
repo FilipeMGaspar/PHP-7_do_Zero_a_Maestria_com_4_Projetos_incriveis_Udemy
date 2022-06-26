26c6633ffc5e2c9a3d3c50fb1d3e3e34a6e56948
fipewmc <fipewmc2@gmail.com>
2022-06-26 08:22:38 +0100

    Criando o banco de dados - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/321_Criando_o_banco_de_dados/config/connection.php
+++ Curso_PHP7/secao17/321_Criando_o_banco_de_dados/config/connection.php
@@ -1,2 +1,13 @@
 <?php
-    
+    $hots = "localhost";
+    $dbname = "agenda";
+    $user = "root";
+    $pass = "";
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass); // Ligação á base de dados
+    }catch (PDOException $e) {
+        // erro na conexão
+        $error = $e->getMessage();
+        echo "Erro: $error";
+    }
