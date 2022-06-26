365228b576206b5b79d8ea2caf48e993c7f6d01a
fipewmc <fipewmc2@gmail.com>
2022-06-26 08:14:37 +0100

    Criando o banco de dados - Seção 17 - Agenda Projeto 2

---
+++ Curso_PHP7/secao17/321_Criando_o_banco_de_dados/index.php
@@ -0,0 +1,8 @@
+<?php
+   require_once "templates/header.php";
+?>
+    <h1>Primeiro teste na agenda</h1>
+    <i class="fa-solid fa-caravan"></i>
+<?php
+    require_once "templates/footer.php";
+?>
