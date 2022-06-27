cbab89d21818aaf7b8f4a72af975cd371dab2ba3
fipewmc <fipewmc2@gmail.com>
2022-06-27 12:09:30 +0100

    Criando arquivo de processamento - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/322_Criando_arquivo_de_processamento/config/process.php
+++ Curso_PHP7/secao17/322_Criando_arquivo_de_processamento/config/process.php
@@ -2,3 +2,10 @@
     session_start(); // inicio da sesão
 
     require_once("connection.php");
+    require_once("url.php");
+
+    /*Seleção de todos os dados da tabela contacts */
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query); // prepare da query a base de dados
