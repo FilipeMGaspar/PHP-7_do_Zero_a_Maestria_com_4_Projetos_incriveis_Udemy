a67216de2317a169902f4c4e357b5e35fbc05cab
fipewmc <fipewmc2@gmail.com>
2022-06-29 14:32:56 +0100

    Tela de registro de contato - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/325_Tela_de_registro_de_contato/show.php
+++ Curso_PHP7/secao17/325_Tela_de_registro_de_contato/show.php
@@ -6,7 +6,10 @@
     <div class="container" id="view-contact-container">
         <h1 id="main-title"><?= $contact["name"] ?></h1>
         <p class="bold">Telefone: </p>
-        <p><?= $contact["name"] ?></p>
+        <p><?= $contact["phone"] ?></p>
+
+        <p class="bold">Observações: </p>
+        <p><?= $contact["observations"] ?></p>
     </div>
    </div>
 
