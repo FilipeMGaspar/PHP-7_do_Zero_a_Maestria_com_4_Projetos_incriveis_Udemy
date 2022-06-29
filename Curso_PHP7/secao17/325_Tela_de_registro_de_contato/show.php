1ed498c714a61c1d48d71ce7a6a20b505ee4a3e8
fipewmc <fipewmc2@gmail.com>
2022-06-29 14:20:47 +0100

    Tela de registro de contato - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/325_Tela_de_registro_de_contato/show.php
+++ Curso_PHP7/secao17/325_Tela_de_registro_de_contato/show.php
@@ -2,6 +2,11 @@
    require_once "templates/header.php";
 ?>
    
+   <div>
+    <div class="container" id="view-contact-container">
+        <h1 id="main-title"><?= $contact["name"] ?></h1>
+    </div>
+   </div>
 
 <?php
     require_once "templates/footer.php";
