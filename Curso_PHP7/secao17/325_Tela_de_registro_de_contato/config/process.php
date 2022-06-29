a1ea48255988ed8bd134f2cd6d7268df160a8fdd
fipewmc <fipewmc2@gmail.com>
2022-06-29 14:03:39 +0100

    Tela de registro de contato - Seção 17 - Agenda Projeto 2

---
+++ Curso_PHP7/secao17/325_Tela_de_registro_de_contato/config/process.php
@@ -0,0 +1,17 @@
+<?php
+    session_start(); // inicio da sesão
+
+    require_once("connection.php");
+    require_once("url.php");
+
+    $contacts = []; // iniciando o contacts como array vazio
+
+    /*Seleção de todos os dados da tabela contacts */
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query); // prepare da query a base de dados
+
+    $stmt->execute(); // Execução da query na base de dados
+
+    $contacts = $stmt->fetchAll(); // Recolha de todos os dados vindos do select
