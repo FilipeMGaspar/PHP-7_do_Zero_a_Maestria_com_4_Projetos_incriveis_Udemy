c56f5a6683d07519dc6a969d94d04007abc1b8a2
fipewmc <fipewmc2@gmail.com>
2022-06-30 14:44:53 +0100

    Criando tela de adicionar contato - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/327_Criando_tela_de_adicionar_contato/create.php
+++ Curso_PHP7/secao17/327_Criando_tela_de_adicionar_contato/create.php
@@ -2,6 +2,7 @@
    require_once "templates/header.php";
 ?>
     <div class="container">
+    <?php require_once "templates/backbtn.php"; ?>
         <h1 id="main-title">Adicionar novo contato</h1>
     </div>
 <?php
