eba652e95920d9d696f3ea0d278e2622fc8cd263
fipewmc <fipewmc2@gmail.com>
2022-07-01 12:36:58 +0100

    Criando formulário de edição - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/329_Criando_formulario_de_edicao/edit.php
+++ Curso_PHP7/secao17/329_Criando_formulario_de_edicao/edit.php
@@ -3,14 +3,15 @@
 ?>
     <div class="container">
     <?php require_once "templates/backbtn.php"; ?>
-        <h1 id="main-title">Adicionar novo contato</h1>
+        <h1 id="main-title">Edição de contato</h1>
 
         <form action="<?= $BASE_URL ?>config/process.php" method="post" id="create-form">
-            <input type="hidden" name="type" value="create">
+            <input type="hidden" name="type" value="edit">
+            <input type="hidden" name="id" value="<?= $contact['id'] ?>">
            
             <div class="form-group">
                 <label for="name">Nome:</label>
-                <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome" required>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome"  value="<?= $contact['name']?>" required>
             </div>
 
             <div class="form-group">
@@ -23,7 +24,7 @@
                 <textarea type="text" name="observations" id="observations" class="form-control" placeholder="Insira observações" rows="3"></textarea>
             </div>
 
-            <button id="btn_forms" type="submit" class="btn btn-primary">Adicionar</button>
+            <button id="btn_forms" type="submit" class="btn btn-primary">Atualizar</button>
         </form>
     </div>
 <?php
