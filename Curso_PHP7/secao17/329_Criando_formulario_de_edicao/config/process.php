f508b416cc017dd58e54d9a884ec33ccc37d0e94
fipewmc <fipewmc2@gmail.com>
2022-07-01 12:28:54 +0100

    Criando formulário de edição - Seção 17 - Agenda Projeto 2

---
+++ Curso_PHP7/secao17/329_Criando_formulario_de_edicao/config/process.php
@@ -0,0 +1,78 @@
+<?php
+    session_start(); // inicio da sesão
+
+    require_once("connection.php");
+    require_once("url.php");
+
+    $data = $_POST;
+
+    // Modificações no Banco
+    if(!empty($data)){
+              
+        // Criar contato
+        if($data["type"] === "create") {
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato criado com sucesso!";
+
+            }catch (PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+        }
+
+        // Redirect Home -> redireciona para a home page
+        header("Location:" . $BASE_URL . "../index.php");
+
+    // Seleção de dados
+    } else {
+
+        $id;
+
+        if(!empty($_GET)) {
+            $id = $_GET["id"];
+        }
+        // Retorna o dado de um contato
+        if(!empty($id)){
+           
+            $query = "SELECT * FROM contacts WHERE id = :id"; // Query sql
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(":id", $id);
+    
+            $stmt->execute();
+    
+            $contact = $stmt->fetch();//REtorna apenas um resultado
+    
+        } else {
+            // Retorna todos os contatos
+            $contacts = []; // iniciando o contacts como array vazio
+    
+            /*Seleção de todos os dados da tabela contacts */
+            $query = "SELECT * FROM contacts";
+    
+            $stmt = $conn->prepare($query); // prepare da query a base de dados
+    
+            $stmt->execute(); // Execução da query na base de dados
+    
+            $contacts = $stmt->fetchAll(); // Recolha de todos os dados vindos do select
+        }
+        
+    }
+
+    // Fechar Conexão
+    $conn = null;
