9fce0750274918e7cebe927143a07d75835c87e1
fipewmc <fipewmc2@gmail.com>
2022-07-01 12:31:57 +0100

    Criando formulário de edição - Seção 17 - Agenda Projeto 2

--- Curso_PHP7/secao17/329_Criando_formulario_de_edicao/edit.php
+++ Curso_PHP7/secao17/329_Criando_formulario_de_edicao/edit.php
@@ -1,1 +1,31 @@
 <?php
+   require_once "templates/header.php";
+?>
+    <div class="container">
+    <?php require_once "templates/backbtn.php"; ?>
+        <h1 id="main-title">Adicionar novo contato</h1>
+
+        <form action="<?= $BASE_URL ?>config/process.php" method="post" id="create-form">
+            <input type="hidden" name="type" value="create">
+           
+            <div class="form-group">
+                <label for="name">Nome:</label>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome" required>
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Telefone:</label>
+                <input type="text" name="phone" id="phone" class="form-control" placeholder="Digite o telefone" required>
+            </div>
+
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" name="observations" id="observations" class="form-control" placeholder="Insira observações" rows="3"></textarea>
+            </div>
+
+            <button id="btn_forms" type="submit" class="btn btn-primary">Adicionar</button>
+        </form>
+    </div>
+<?php
+    require_once "templates/footer.php";
+?>
