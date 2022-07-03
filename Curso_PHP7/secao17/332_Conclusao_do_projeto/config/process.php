5fded13201ce4891407e600182877f2a0354246e
fipewmc <fipewmc2@gmail.com>
2022-07-03 09:42:58 +0100

    Removendo contatos - Seção 17 - Agenda Projeto 2

---
+++ Curso_PHP7/secao17/332_Conclusao_do_projeto/config/process.php
@@ -0,0 +1,119 @@
+<?php
+    session_start(); // inicio da sesão
+
+    require_once("connection.php");
+    require_once("url.php");
+
+    $data = $_POST;
+
+    // Modificações no Banco
+    if(!empty($data)){
+              
+        // Criar contato
+        if($data["type"] === "create") {
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato criado com sucesso!";
+
+            }catch (PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+        } else if($data["type"] === "edit") {
+
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts SET name = :name, phone = :phone, observations  = :observations WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato atualizado com sucesso!";
+
+            }catch (PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+        } else if($data["type"] === "delete") {
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":id", $id);
+
+            try {
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+
+            }catch (PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
+        }
+
+        // Redirect Home -> redireciona para a home page
+        header("Location:" . $BASE_URL . "../index.php");
+
+    // Seleção de dados
+    } else {
+
+        $id;
+
+        if(!empty($_GET)) {
+            $id = $_GET["id"];
+        }
+        // Retorna o dado de um contato
+        if(!empty($id)){
+           
+            $query = "SELECT * FROM contacts WHERE id = :id"; // Query sql
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(":id", $id);
+    
+            $stmt->execute();
+    
+            $contact = $stmt->fetch();//REtorna apenas um resultado
+    
+        } else {
+            // Retorna todos os contatos
+            $contacts = []; // iniciando o contacts como array vazio
+    
+            /*Seleção de todos os dados da tabela contacts */
+            $query = "SELECT * FROM contacts";
+    
+            $stmt = $conn->prepare($query); // prepare da query a base de dados
+    
+            $stmt->execute(); // Execução da query na base de dados
+    
+            $contacts = $stmt->fetchAll(); // Recolha de todos os dados vindos do select
+        }
+        
+    }
+
+    // Fechar Conexão
+    $conn = null;
---
+++ Curso_PHP7/secao17/332_Conclusao_do_projeto/edit.php
@@ -0,0 +1,32 @@
+<?php
+   require_once "templates/header.php";
+?>
+    <div class="container">
+    <?php require_once "templates/backbtn.php"; ?>
+        <h1 id="main-title">Edição de contato</h1>
+
+        <form action="<?= $BASE_URL ?>config/process.php" method="post" id="create-form">
+            <input type="hidden" name="type" value="edit">
+            <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+           
+            <div class="form-group">
+                <label for="name">Nome:</label>
+                <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome"  value="<?= $contact['name']?>" required>
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Telefone:</label>
+                <input type="text" name="phone" id="phone" class="form-control" placeholder="Digite o telefone"  value="<?= $contact['phone'] ?>" required>
+            </div>
+
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" name="observations" id="observations" class="form-control" placeholder="Insira observações" rows="3"><?= $contact['observations'] ?></textarea>
+            </div>
+
+            <button id="btn_forms" type="submit" class="btn btn-primary">Atualizar</button>
+        </form>
+    </div>
+<?php
+    require_once "templates/footer.php";
+?>
