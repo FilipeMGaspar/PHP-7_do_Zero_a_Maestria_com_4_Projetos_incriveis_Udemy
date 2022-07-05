16382be834ea0477b566d2c925cd7bad74b007e4
fipewmc <fipewmc2@gmail.com>
2022-07-05 20:27:49 +0100

    Criando a classe - Seção 17 - Design patterns e padrões de desenvolvimento

--- Curso_PHP7/secao18/338_Criando_a_classe/index.php
+++ Curso_PHP7/secao18/338_Criando_a_classe/index.php
@@ -1,3 +1,9 @@
 <?php
-    require_once "db.php";
+    //require_once "db.php";
+  // require_once "Car.php";
+
+   // $bmw = new Car("BMW", 15023, "Azul");
+
+    echo "Olá";
+    //print_r($bmw);
 ?>
