e3a667649e7a721f4d01a70339c01dcaf5d5c233
fipewmc <fipewmc2@gmail.com>
2022-07-07 10:58:15 +0100

    Criando a classe DAO - Seção 17 - Design patterns e padrões de desenvolvimento

--- Curso_PHP7/secao18/339_Criando_a_classe_DAO/dao/CarDAO.php
+++ Curso_PHP7/secao18/339_Criando_a_classe_DAO/dao/CarDAO.php
@@ -1,2 +1,16 @@
 <?php
-    
+
+use CarDAO as GlobalCarDAO;
+
+    require_once "models/Car.php";
+
+    class CarDAO implements CarDAO {
+        public function findAll() {
+            
+        }
+
+        public function create(Car $car)
+        {
+            
+        }
+    }
