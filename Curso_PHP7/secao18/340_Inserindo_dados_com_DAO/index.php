c97f65a0a0b1e05a7275b3a8d13da445fa3b503a
fipewmc <fipewmc2@gmail.com>
2022-07-07 11:07:46 +0100

    Inserindo dados com DAO - Seção 17 - Design patterns e padrões de desenvolvimento

--- Curso_PHP7/secao18/340_Inserindo_dados_com_DAO/index.php
+++ Curso_PHP7/secao18/340_Inserindo_dados_com_DAO/index.php
@@ -3,3 +3,20 @@
 
 ?>
 
+<h1>Insira um carro:</h1>
+<form action="process.php" method="post">
+    <div>
+        <label for="brand">Marca: </label>
+        <input type="text" name="brand" placeholder="Marca do carro">
+    </div>
+
+    <div>
+        <label for="km">Kilometragem: </label>
+        <input type="text" name="km" placeholder="Insira a Kilometragem">
+    </div>
+
+    <div>
+        <label for="color">Cor: </label>
+        <input type="text" name="color" placeholder="Cor do carro">
+    </div>
+</form>
