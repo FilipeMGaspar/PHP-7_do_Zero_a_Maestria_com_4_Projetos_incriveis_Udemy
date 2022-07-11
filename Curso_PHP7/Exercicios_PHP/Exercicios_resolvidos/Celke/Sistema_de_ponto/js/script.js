b53cc346c1dcd1d1fb62f73439f6cfd6b7f5a81e
fipewmc <fipewmc2@gmail.com>
2022-07-11 14:59:28 +0100

    Sistema de ponto para cadastrar horário de entrada e saída - Celke

--- Curso_PHP7/Exercicios_PHP/Exercicios_resolvidos/Celke/Sistema_de_ponto/js/script.js
+++ Curso_PHP7/Exercicios_PHP/Exercicios_resolvidos/Celke/Sistema_de_ponto/js/script.js
@@ -1,7 +1,9 @@
 var apHorario = document.querySelector("#horario")
 
 function atualizarHoratio () {
-
+   new Date().toLocaleDateString("pt-pt", {
+        timeZone: "Europe/Lisbon"
+   })
 }
 
 setInterval(atualizarHoratio, 1000)
