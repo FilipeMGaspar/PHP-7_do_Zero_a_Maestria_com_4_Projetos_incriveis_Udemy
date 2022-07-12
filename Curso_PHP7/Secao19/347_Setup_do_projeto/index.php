ac5e41568ac09111b978a9524b342ff6099dc459
fipewmc <fipewmc2@gmail.com>
2022-07-12 12:19:12 +0100

    Setup do projeto - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
+++ Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar - Projeto 3 - Hora de Codar</title>
+   
     <!-- Icon -->
     <link rel="shortcut icon" href="img/moviestar.ico" type="image/x-icon">
+
+    <!-- font-awesome - from cdnjs  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- BootStrap  - from cdnjs  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.css" integrity="sha512-F5fn88UDJWd0T1GwpnIpbuFuSxCgXNnJhIihkiWeqYUyLt3oPUaYzL774VjL7eGj8ktZCZQS//txAxgokiz1Lw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -14,7 +18,6 @@
     <!-- CSS do projeto -->
     <link rel="stylesheet" href="css/style.css">
 
-    <!--  -->
 </head>
 <body>
     <h1>Testando MovieStar</h1>
