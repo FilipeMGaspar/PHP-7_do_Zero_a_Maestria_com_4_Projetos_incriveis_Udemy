400f418fe71c4583debb735098c1efdea35c8571
fipewmc <fipewmc2@gmail.com>
2022-07-12 11:50:57 +0100

    Criando banco de dados - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/346_Criando_banco_de_dados/Criando_banco_de_dados.sql
+++ Curso_PHP7/Secao19/346_Criando_banco_de_dados/Criando_banco_de_dados.sql
@@ -21,3 +21,13 @@ CREATE TABLE movies (
 	users_id INT(11) UNSIGNED,
 	FOREIGN KEY(users_id) REFERENCES users(id)
 );
+
+CREATE TABLE reviews(
+	id INT(11) UNSIGN AUTO_INCREMENT PRIMARY KEY,
+	rating INT,
+	review TEXT,
+	users_id INT(11) UNSIGNED,
+	movies_id INT(11) UNSIGNED,
+	FOREIGN KEY(users_id) REFERENCES users(id),
+	FOREIGN KEY(movies_id) REFERENCES movies(id)
+);
