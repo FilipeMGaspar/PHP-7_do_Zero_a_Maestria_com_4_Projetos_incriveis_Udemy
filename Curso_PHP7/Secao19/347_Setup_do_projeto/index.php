d8384eca0be8f88bf2b0b2d79c4cfdfeecd75c2b
fipewmc <fipewmc2@gmail.com>
2022-07-12 12:17:51 +0100

    Setup do projeto - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
+++ Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
@@ -21,5 +21,7 @@
 
     <!-- JavaScript BootStrap  - from cdnjs  -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/js/bootstrap.js" integrity="sha512-kZzIlRPOhobf+l/HSLHKu5E6UVe34lDZKtQzNEMHRNdctaVhwIPVruz1ahHqtMUTuYxJaSmVIZyWjV7jlfpAyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
