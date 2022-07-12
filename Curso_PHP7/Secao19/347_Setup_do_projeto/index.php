5c4cd94bd55fe150b3e81f2af287ef064fc058d7
fipewmc <fipewmc2@gmail.com>
2022-07-12 12:07:14 +0100

    Setup do projeto - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
+++ Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar - Projeto 3 - Hora de Codar</title>
+    <!-- Icon -->
+    <link rel="shortcut icon" href="img/moviestar.ico" type="image/x-icon">
 </head>
 <body>
     
