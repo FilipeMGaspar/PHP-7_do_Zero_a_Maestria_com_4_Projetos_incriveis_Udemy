895ee625656a9246abd885eb6ddf17678c4ec5e7
fipewmc <fipewmc2@gmail.com>
2022-07-12 12:16:25 +0100

    Setup do projeto - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
+++ Curso_PHP7/Secao19/347_Setup_do_projeto/index.php
@@ -8,10 +8,18 @@
     <!-- Icon -->
     <link rel="shortcut icon" href="img/moviestar.ico" type="image/x-icon">
 
+    <!-- BootStrap  - from cdnjs  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.css" integrity="sha512-F5fn88UDJWd0T1GwpnIpbuFuSxCgXNnJhIihkiWeqYUyLt3oPUaYzL774VjL7eGj8ktZCZQS//txAxgokiz1Lw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- CSS do projeto -->
     <link rel="stylesheet" href="css/style.css">
+
+    <!--  -->
 </head>
 <body>
     <h1>Testando MovieStar</h1>
+
+    <!-- JavaScript BootStrap  - from cdnjs  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/js/bootstrap.js" integrity="sha512-kZzIlRPOhobf+l/HSLHKu5E6UVe34lDZKtQzNEMHRNdctaVhwIPVruz1ahHqtMUTuYxJaSmVIZyWjV7jlfpAyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
