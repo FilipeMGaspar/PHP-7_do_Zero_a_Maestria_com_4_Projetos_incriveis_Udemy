4a0bb1ea36874ccb2db8e5b954624b00cc11c74b
fipewmc <fipewmc2@gmail.com>
2022-07-14 09:29:10 +0100

    Criando a conexão com o banco - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/Secao19/348_Criando_a_conexao_com_o_banco/db.php
+++ Curso_PHP7/Secao19/348_Criando_a_conexao_com_o_banco/db.php
@@ -6,3 +6,6 @@
     $pass = "";
 
     $conn = new PDO("mysql:host=$host;dabname=$dbname", $user, $pass);
+
+    // Habilitar erros PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
