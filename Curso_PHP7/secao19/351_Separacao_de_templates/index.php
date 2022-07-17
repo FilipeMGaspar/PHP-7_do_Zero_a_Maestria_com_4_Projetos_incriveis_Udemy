6bc34a971dc34cdd0b87ed1a2bc706ab4bae3931
fipewmc <fipewmc2@gmail.com>
2022-07-17 12:29:57 +0100

    Separação de templates - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/351_Separacao_de_templates/index.php
+++ Curso_PHP7/secao19/351_Separacao_de_templates/index.php
@@ -1,31 +1,4 @@
-<?php
-    require_once "globals.php";
-    require_once "db.php"
-?>
-<!DOCTYPE html>
-<html lang="pt-pt">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MovieStar - Projeto 3 - Hora de Codar</title>
-   
-    <!-- Icon -->
-    <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico" type="image/x-icon">
 
-    <!-- font-awesome - from cdnjs  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- BootStrap  - from cdnjs  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/css/bootstrap.css" integrity="sha512-F5fn88UDJWd0T1GwpnIpbuFuSxCgXNnJhIihkiWeqYUyLt3oPUaYzL774VjL7eGj8ktZCZQS//txAxgokiz1Lw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- CSS do projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
-
-</head>
-<body>
-    
-    <header>
         <nav id="main-navbar" class="navbar navbar-expand-lg">
             <a href="<?= $BASE_URL ?>" class="navbar-brand">
                 <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
