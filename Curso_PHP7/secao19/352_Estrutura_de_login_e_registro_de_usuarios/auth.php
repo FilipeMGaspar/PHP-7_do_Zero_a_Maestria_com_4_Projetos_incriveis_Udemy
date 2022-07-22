2bcf1c5903c93aeaaa888e32ce3171ade29092d9
fipewmc <fipewmc2@gmail.com>
2022-07-22 18:14:12 +0100

    Estrutura de login e registro de usuários - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/352_Estrutura_de_login_e_registro_de_usuarios/auth.php
+++ Curso_PHP7/secao19/352_Estrutura_de_login_e_registro_de_usuarios/auth.php
@@ -4,8 +4,19 @@
 
     <div id="main-container" class="container-fluid">
         <div class="col-md-12">
-            <div class="row">
-                
+            <div class="row" id="auth-row">
+                <div class="col-md-4" id="login-container">
+                    <h2>Entrar</h2>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="col-md-4" id="register-container">
+                    <h2>Criar conta</h2>
+                </div>
             </div>
         </div>
     </div>
