31614cc88c36100146eff626a95294be9050b271
fipewmc <fipewmc2@gmail.com>
2022-07-25 10:54:45 +0100

    Criando o model de usuárioe usuário - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/354_Criando_o_model_de_usuario/dao/userDao.php
+++ Curso_PHP7/secao19/354_Criando_o_model_de_usuario/dao/userDao.php
@@ -2,5 +2,44 @@
     require_once "../models/user.php";
 
     class UserDAO implements UserDAOInterface {
+        public function buildUser($data){
+
+        }
+
+        public function create(User $user, $authUser = false){
+
+        }
+
+        public function update(User $user) {
+
+        }
+       
+        public function verifyToken($protected = false){
+
+        }
+
+        public function setTokenToSession($token, $redirect = true){
+
+        }
+
+        public function authenticateUser($email, $password){
+
+        }
+
+        public function findByEmail($email) {
+
+        }
+
+        public function findById($id) {
+
+        }
+
+        public function findByToken($token) {
+
+        }
+
+        public function changePassword(User $user){
+
+        }
         
     }
