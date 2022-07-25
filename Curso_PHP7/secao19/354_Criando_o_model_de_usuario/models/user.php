6fa3e36a91a734a4898034d49138de35aa5ec0af
fipewmc <fipewmc2@gmail.com>
2022-07-25 10:48:27 +0100

    Criando o model de usuárioe usuário - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/354_Criando_o_model_de_usuario/models/user.php
+++ Curso_PHP7/secao19/354_Criando_o_model_de_usuario/models/user.php
@@ -19,4 +19,7 @@
         public function findByToken($token);
         public function verifyToken($protected = false);
         public function setTokenToSession($token, $redirect = true);
+        public function authenticateUser($email, $password);
+        public function findByEmail($email);
+        public function findById($id);
     }
