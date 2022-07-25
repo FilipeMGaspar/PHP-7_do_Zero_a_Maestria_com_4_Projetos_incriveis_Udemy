ea046edcd5292ed7ecf9031cd16ea9dd2c87571d
fipewmc <fipewmc2@gmail.com>
2022-07-25 09:18:58 +0100

    Criando o model de usuárioe usuário - Seção 19 - MovieStar - Projeto 3

---
+++ Curso_PHP7/secao19/354_Criando_o_model_de_usuario/db.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = "localhost";
+    $dbname = "moviestar";
+    $user = "root";
+    $pass = "";
+
+    $conn = new PDO("mysql:host=$host;dabname=$dbname", $user, $pass);
+
+    // Habilitar erros PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
