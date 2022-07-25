eaac9cebf1205e2ee9a0146a6a73166333b07ad7
fipewmc <fipewmc2@gmail.com>
2022-07-25 10:41:19 +0100

    Criando o model de usuárioe usuário - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/354_Criando_o_model_de_usuario/models/user.php
+++ Curso_PHP7/secao19/354_Criando_o_model_de_usuario/models/user.php
@@ -1,2 +1,13 @@
 <?php
-    
+    class User {
+        public $id;
+        public $name;
+        public $lastName;
+        public $email;
+        public $password;
+        public $image;
+        public $bio;
+        public $token;
+
+        
+    }
