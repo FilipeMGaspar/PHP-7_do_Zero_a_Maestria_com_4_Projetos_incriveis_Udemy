b939fc4680f5510256d73738be4fe10c97c1948c
fipewmc <fipewmc2@gmail.com>
2022-07-27 09:48:14 +0100

    Iniciando a autenticação - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/355_Iniciando_a_autenticacao/auth_process.php
+++ Curso_PHP7/secao19/355_Iniciando_a_autenticacao/auth_process.php
@@ -1,3 +1,11 @@
 <?php
+    require_once "globals.php";
+    require_once "db.php";
     require_once "models/User.php";
     require_once "dao/UserDAO.php";
+   
+    // Verifica o tipo do formulário ..: Register :.. ou ..: Login :..
+
+    $type = filter_input(INPUT_POST, "type"); // Evita inserção de dados maliciosos pelo utilizador
+
+    echo $type;
