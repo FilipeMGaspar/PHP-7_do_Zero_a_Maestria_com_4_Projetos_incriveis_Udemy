cfa2c5acfa38064abdd2949970e0ede77c2e11a6
fipewmc <fipewmc2@gmail.com>
2022-08-06 15:57:11 +0100

    Registro de usuário - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/359_Registro_de_usuario/editprofile.php
+++ Curso_PHP7/secao19/359_Registro_de_usuario/editprofile.php
@@ -1,2 +1,11 @@
 <?php
-    echo "Olá Parvo!";
+    require_once "templates/header.php";
+?>
+
+    <div id="main-container" class="container-fluid">
+        <h1>Edição de perfil de utilizador</h1>
+    </div>
+
+<?php
+    require_once "templates/footer.php";
+?>
