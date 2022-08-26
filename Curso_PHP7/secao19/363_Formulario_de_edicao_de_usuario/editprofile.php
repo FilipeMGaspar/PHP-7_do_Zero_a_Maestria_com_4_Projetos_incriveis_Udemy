02a7caab72a6dc12c85e8afa2c11071616263783
fipewmc <fipewmc2@gmail.com>
2022-08-26 10:26:21 +0100

    Formulário de edição de usuário - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/363_Formulario_de_edicao_de_usuario/editprofile.php
+++ Curso_PHP7/secao19/363_Formulario_de_edicao_de_usuario/editprofile.php
@@ -3,9 +3,12 @@
     require_once "models/User.php";
     require_once "dao/UserDAO.php";
 
+    $user = new User();
     $userDao = new UserDAO($conn, $BASE_URL);
 
     $userData = $userDao->verifyToken(true);
+
+    $fullName = $user->getFullName($userData);
 ?>
 
     <div id="main-container" class="container-fluid">
@@ -14,7 +17,7 @@
                 <input type="hidden" name="type" value="update">
                 <div class="row">
                     <div class="col-md-4">
-                        <h1><?= ?></h1>
+                        <h1><?= $fullName ?></h1>
                     </div>
                 </div>
             </form>
