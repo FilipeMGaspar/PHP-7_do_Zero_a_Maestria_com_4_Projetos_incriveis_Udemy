bc87c98be4218668689abd0fa9ff28d6d816bb54
fipewmc <fipewmc2@gmail.com>
2022-09-08 12:35:33 +0100

    Upload de imagem do usuário - Seção 19 - MovieStar - Projeto 3

---
+++ Curso_PHP7/secao19/366_Upload_de_imagem_do_usuario/user_process.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once "globals.php";
+require_once "db.php";
+require_once "models/User.php";
+require_once "dao/UserDAO.php";
+require_once "models/Message.php";
+
+
+$message = new Message($BASE_URL); // Instanciamento do sistema de mensagem passando a base url
+
+$userDao = new UserDAO($conn, $BASE_URL); // Instacia do UserDAO
+
+// Resgata o tipo do formulário
+$type = filter_input(INPUT_POST, "type"); // Evita inserção de dados maliciosos pelo utilizador
+
+//Atualização do utilizador
+if($type === "update") {
+
+    //Resgata dados do utilizador
+    $userData = $userDao->verifyToken();
+    
+    //Recebe dados do formulário através do Post
+    $name = filter_input(INPUT_POST, "name");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $email = filter_input(INPUT_POST, "email");
+    $bio = filter_input(INPUT_POST, "bio");
+
+    // Criação de um novo objeto de utilizador
+    $user = new User();
+
+    // Preencher os dados do utilizador
+    $userData->name = $name;
+    $userData->lastname = $lastname;
+    $userData->email = $email;
+    $userData->bio = $bio;
+
+    $userDao->update($userData);
+
+  //atualizar a password
+} else if($type === "changepassword") { 
+
+} else {
+    $message->setMessage("informações inválidas!", "error", "");
+}
