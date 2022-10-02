3c9c72132400029b6841f8f213e0b77760626a7b
fipewmc <fipewmc2@gmail.com>
2022-10-02 18:22:53 +0100

    Formulário de adição de filme - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/368_Formulario_de_adicao_de_filme/newmovie.php
+++ Curso_PHP7/secao19/368_Formulario_de_adicao_de_filme/newmovie.php
@@ -15,8 +15,14 @@
         <div class="offset-md-4 col-md-4 new-movie-container">
             <h1 class="page-title">Adicionar Filme</h1>
             <p class="page-description">Pode adicionar uma critica e partilha-la com o mundo!</p>
+            
             <form action="<?= $BASE_URL ?>movie_process.php" id="add-movie-form" method="POST" enctype="multipart/form-data">
-                
+                <input type="hidden" name="type" value="create">
+
+                <div class="form-group">
+                    <label for="title">Título</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Digite o título do filme">
+                </div>
             </form>
         </div>
     </div>
