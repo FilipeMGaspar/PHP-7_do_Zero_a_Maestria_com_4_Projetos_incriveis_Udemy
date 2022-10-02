9e51d46be931adeb30a58183d31899f8d38a45e8
fipewmc <fipewmc2@gmail.com>
2022-10-02 18:33:55 +0100

    Formulário de adição de filme - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/368_Formulario_de_adicao_de_filme/newmovie.php
+++ Curso_PHP7/secao19/368_Formulario_de_adicao_de_filme/newmovie.php
@@ -46,6 +46,16 @@
                     </select>
                 </div>
 
+                
+                <div class="form-group">
+                    <label for="trailer">Trailer:</label>
+                    <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Insira o link do trailer">
+                </div>
+
+                <div class="form-group">
+                    <label for="trailer">Trailer:</label>
+                    
+                </div>
             </form>
         </div>
     </div>
