412e13f7d69e4e58826b98a533814c7bc8796938
fipewmc <fipewmc2@gmail.com>
2022-10-06 14:22:46 +0100

    Criando Model de filme - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/369_Criando_Model_de_filme/models/Movie.php
+++ Curso_PHP7/secao19/369_Criando_Model_de_filme/models/Movie.php
@@ -1,1 +1,15 @@
 <?php
+
+class Movie {
+
+    public $id;
+    public $title;
+    public $description;
+    public $image;
+    public $trailer;
+    public $category;
+    public $length;
+    public $users_id;
+
+    
+}
