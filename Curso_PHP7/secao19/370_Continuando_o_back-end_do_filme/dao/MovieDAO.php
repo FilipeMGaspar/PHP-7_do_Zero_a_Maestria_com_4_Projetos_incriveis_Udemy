48ddd3831221a57257c8fd5b3ea77cc0683d18b3
fipewmc <fipewmc2@gmail.com>
2022-10-15 08:09:24 +0100

    Continuando o back-end do filme - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/370_Continuando_o_back-end_do_filme/dao/MovieDAO.php
+++ Curso_PHP7/secao19/370_Continuando_o_back-end_do_filme/dao/MovieDAO.php
@@ -16,4 +16,45 @@
             $this->url = $url;
             $this->message = new Message($url);
         }
+
+        public function buildMovie($data){
+
+        }
+
+        public function findAll() {
+
+        }
+
+        public function getLatestMovies() {
+
+        }
+
+        public function getMoviesByCategory($category) {
+
+        }
+
+        public function getMoviesByUsersId($id) {
+
+        }
+
+        public function findByID($id) {
+
+        }
+
+        public function finByTitle($title){
+
+        }
+
+        public function create(Movie $movie) {
+
+        }
+
+        public function update(Movie $movie) {
+
+        }
+
+        public function destroy($id) {
+
+        }
+        
       }
