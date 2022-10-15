039bed20841dfbfed8b59a76e7bac00365edf6b3
fipewmc <fipewmc2@gmail.com>
2022-10-15 08:04:38 +0100

    Continuando o back-end do filme - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/370_Continuando_o_back-end_do_filme/dao/MovieDAO.php
+++ Curso_PHP7/secao19/370_Continuando_o_back-end_do_filme/dao/MovieDAO.php
@@ -1,3 +1,15 @@
 <?php
+
       require_once "models/Movie.php";
       require_once "models/Message.php";
+
+      //Review DAO
+
+      class MovieDAO implements MovieDAOInterface {
+
+        private $conn;
+        private $url;
+        private $message;
+
+        
+      }
