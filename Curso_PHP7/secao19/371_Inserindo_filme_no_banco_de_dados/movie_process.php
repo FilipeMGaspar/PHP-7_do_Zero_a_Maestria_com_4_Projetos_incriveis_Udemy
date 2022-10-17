615ea709914ecadb8a4c771a615e60f8b65307c7
fipewmc <fipewmc2@gmail.com>
2022-10-17 14:43:59 +0100

    Inserindo filme no banco de dados - Seção 19 - MovieStar - Projeto 3

--- Curso_PHP7/secao19/371_Inserindo_filme_no_banco_de_dados/movie_process.php
+++ Curso_PHP7/secao19/371_Inserindo_filme_no_banco_de_dados/movie_process.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once "globals.php";
-require_once "db.php";
-require_once "models/User.php";
-require_once "dao/UserDAO.php";
-require_once "models/Message.php";
+    require_once "globals.php";
+    require_once "db.php";
+    require_once "models/Movie.php";
+    require_once "dao/UserDAO.php";
+    require_once "dao/MovieDAO.php";
+    require_once "models/Message.php";
 
 
-$message = new Message($BASE_URL); // Instanciamento do sistema de mensagem passando a base url
+    $message = new Message($BASE_URL); // Instanciamento do sistema de mensagem passando a base url
